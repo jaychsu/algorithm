2252e624eb1ffdc1775a18b483272b5d61806fd4
Jaych Su <hi@jaych.su>
2017-10-15 23:02:39 +0800

    Refactoring `friendship-service`

--- python/friendship-service.py
+++ python/friendship-service.py
@@ -1,9 +1,9 @@
-EMPTY_SET = set()
-
 class FriendshipService:
+
     def __init__(self):
         self.followers = {}
         self.followings = {}
+        self.NULL_SET = set()
 
     def get_followers(self, user_id):
         return self.getFollowers(user_id)
@@ -15,22 +15,24 @@ class FriendshipService:
     @return: all followers and sort by user_id
     """
     def getFollowers(self, user_id):
-        return [
-            id for id in sorted(self.followers[user_id])
-        ] if user_id in self.followers else []
+        if user_id in self.followers:
+            return [id for id in sorted(self.followers[user_id])]
+        else:
+            return []
 
     """
     @param: user_id: An integer
     @return: all followings and sort by user_id
     """
     def getFollowings(self, user_id):
-        return [
-            id for id in sorted(self.followings[user_id])
-        ] if user_id in self.followings else []
+        if user_id in self.followings:
+            return [id for id in sorted(self.followings[user_id])]
+        else:
+            return []
 
     """
-    @param: to_user_id: An integer
     @param: from_user_id: An integer
+    @param: to_user_id: An integer
     @return: nothing
     """
     def follow(self, to_user_id, from_user_id):
@@ -42,12 +44,12 @@ class FriendshipService:
         self.followers[to_user_id].add(from_user_id)
 
     """
-    @param: to_user_id: An integer
     @param: from_user_id: An integer
+    @param: to_user_id: An integer
     @return: nothing
     """
     def unfollow(self, to_user_id, from_user_id):
-        if to_user_id in self.followings.get(from_user_id, EMPTY_SET):
+        if to_user_id in self.followings.get(from_user_id, self.NULL_SET):
             self.followings[from_user_id].remove(to_user_id)
-        if from_user_id in self.followers.get(to_user_id, EMPTY_SET):
+        if from_user_id in self.followers.get(to_user_id, self.NULL_SET):
             self.followers[to_user_id].remove(from_user_id)
