c2e9abfd8e924910cab0444605f27cae4aa6730f
Jaych Su <hi@jaych.su>
2017-10-20 18:19:17 +0800

    Refactoring `tiny-url`

--- python/232-tiny-url.py
+++ python/232-tiny-url.py
@@ -3,46 +3,40 @@ import re
 
 class TinyUrl:
     def __init__(self):
-        self.char_list = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
-        self.short_id_digit = 6
+        self.char_list = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
         self.host = 'http://tiny.url/'
-        self.longurls = {}
-        self.shortids = {}
+        self.k = 6
+        self.l2s = {}
+        self.s2l = {}
 
-    def generateShortId(self):
-        result = ''
-        for i in range(self.short_id_digit):
-            index = random.randint(0, len(self.char_list) - 1)
-            result += self.char_list[index]
-        return result
-
-    def toShortUrl(self, shortid):
-        if isinstance(shortid, str):
-            return self.host + shortid
-        else:
-            return None
+    def generateHash(self, k):
+        res = ''
+        j, n = 0, len(self.char_list)
+        for i in range(k):
+            j = random.randint(0, n - 1)
+            res += self.char_list[j]
+        return res
 
     """
     @param: url: a long url
     @return: a short url starts with http://tiny.url/
     """
     def longToShort(self, url):
-        if url in self.longurls:
-            return self.toShortUrl(self.longurls[url])
-        shortid = self.generateShortId()
-        while shortid in self.shortids:
-            shortid = self.generateShortId()
-        self.longurls[url] = shortid
-        self.shortids[shortid] = url
-        return self.toShortUrl(shortid)
+        if url in self.l2s:
+            return self.host + self.l2s[url]
+        hash = self.generateHash(self.k)
+        while hash in self.s2l:
+            hash = self.generateHash(self.k)
+        self.l2s[url] = hash
+        self.s2l[hash] = url
+        return self.host + hash
 
     """
     @param: url: a short url starts with http://tiny.url/
     @return: a long url
     """
     def shortToLong(self, url):
-        shortid = re.match(self.host + '([A-Za-z0-9]+)\/?', url)
-        shortid = shortid.group(1) if shortid else None
-        if shortid in self.shortids:
-            return self.shortids[shortid]
-        return None
+        hash = re.match(self.host + '([A-Za-z0-9]+)\/?', url)
+        hash = hash.group(1) if hash else None
+        if hash in self.s2l:
+            return self.s2l[hash]
