c4648675ff698a1190b03e681abbd5c18abb59bf
Jaych Su <hi@jaych.su>
2017-10-22 21:14:38 +0800

    2nd, minimum-size-subarray-sum

--- python/406-minimum-size-subarray-sum.py
+++ python/406-minimum-size-subarray-sum.py
@@ -5,27 +5,26 @@ class Solution:
     @return: an integer representing the minimum size of subarray
     """
     def minimumSize(self, nums, s):
-        n = len(nums)
-        if n == 0:
+        if not nums or len(nums) == 0:
             return -1
-        left = right = total = 0
-        min_size = n + 1
-        while right < n:
-
+        n = len(nums)
+        min_len = n + 1
+        l = r = t = 0
+        while r < n:
             # Keep adding the next int into total until total >= s
-            while right < n and total < s:
-                total += nums[right]
-                right += 1
+            while r < n and t < s:
+                t += nums[r]
+                r += 1
 
             # Terminate iteration if all the children in nums have been added
-            if total < s:
+            if r >= n and t < s:
                 break
 
             # Keep substracting the prev int from total until total < s
-            while left < right and total >= s:
-                total -= nums[left]
-                left += 1
+            while l < r and t >= s:
+                t -= nums[l]
+                l += 1
 
             # Save the min_size
-            min_size = min(min_size, right + 1 - left)
-        return -1 if min_size > n else min_size
+            min_len = min(min_len, r - l + 1)
+        return -1 if min_len > n else min_len
