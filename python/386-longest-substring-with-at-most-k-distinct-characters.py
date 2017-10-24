caaa6d1ec8843496e7baf87a8b9edda04ee583e2
Jaych Su <hi@jaych.su>
2017-10-24 11:40:37 +0800

    2nd, longest-substring-with-at-most-k-distinct-characters

--- python/386-longest-substring-with-at-most-k-distinct-characters.py
+++ python/386-longest-substring-with-at-most-k-distinct-characters.py
@@ -7,21 +7,19 @@ class Solution:
     def lengthOfLongestSubstringKDistinct(self, s, k):
         if not s or not k:
             return 0
-        n = len(s)
-        ans, found = 0, {}
-        i = j = 0 # i for right pointer, j for left pointer
-        while j < n:
+        ans = l = r = 0
+        n, freq = len(s), {}
+        while r < n:
+            while r < n and (s[r] in freq or len(freq) < k):
+                freq[s[r]] = freq.get(s[r], 0) + 1
+                r += 1
 
-            while j < n \
-            and (s[j] in found or len(found) < k):
-                found[s[j]] = found.get(s[j], 0) + 1
-                j += 1
+            # the r here already after the legal char, since `r += 1` in above
+            ans = max(ans, r - l)
 
-            ans = max(ans, j - i) # the j here already after the legal char, since `j += 1` in above
-
-            while i < j and len(found) >= k:
-                found[s[i]] -= 1
-                if found[s[i]] == 0: del found[s[i]]
-                i += 1
-
+            while l < r and len(freq) >= k:
+                freq[s[l]] -= 1
+                if freq[s[l]] == 0:
+                    del freq[s[l]]
+                l += 1
         return ans
