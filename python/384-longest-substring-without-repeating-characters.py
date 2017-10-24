59f6629697f6d8d6f151744cde8b1ecf260956e7
Jaych Su <hi@jaych.su>
2017-10-24 11:19:33 +0800

    2nd, longest-substring-without-repeating-characters

--- python/384-longest-substring-without-repeating-characters.py
+++ python/384-longest-substring-without-repeating-characters.py
@@ -6,11 +6,15 @@ class Solution:
     def lengthOfLongestSubstring(self, s):
         if not s:
             return 0
-        ans = left = 0
-        last = {}
-        for i in range(len(s)):
-            if s[i] in last and last[s[i]] + 1 > left:
-                left = last[s[i]] + 1
-            last[s[i]] = i
-            ans = max(ans, i + 1 - left)
+        n, last = len(s), {}
+        ans = l = 0
+        for r in range(n):
+            # If find the repeated one and position great than before, set the valid left index
+            if s[r] in last and last[s[r]] + 1 > l: l = last[s[r]] + 1
+
+            # Record the last time index of each character
+            last[s[r]] = r
+
+            # Every time if the interval between left and right is the bigger one, that's answer
+            ans = max(ans, r + 1 - l)
         return ans
