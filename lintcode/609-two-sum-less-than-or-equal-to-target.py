d3fc8d8f45a0c34812cb73ca3918731bb8c334c5
Jaych Su <hi@jaych.su>
2017-10-30 20:38:40 +0800

    2nd, two-sum-less-than-or-equal-to-target

--- lintcode/609-two-sum-less-than-or-equal-to-target.py
+++ lintcode/609-two-sum-less-than-or-equal-to-target.py
@@ -5,19 +5,18 @@ class Solution:
     @return: an integer
     """
     def twoSum5(self, nums, target):
-        if not nums \
-        or len(nums) < 2:
+        if not nums or len(nums) < 2:
             return 0
         ans = sum = 0
         l, r = 0, len(nums) - 1
         nums.sort()
         while l < r:
             sum = nums[l] + nums[r]
-            if sum > target:
-                r -= 1
-            else:
-                # since the items from nums[l+1] to nums[r] meet the demands,
-                # `r-(l+1)+1 = r-l`, `+1` means including nums[l+1] itself
+            if sum <= target:
+                # the count of connections from `l` to `r`
+                # e.g, from 1 to 4, 1-2, 1-3, 1-4, got 3 connections
                 ans += r - l
                 l += 1
+            else:
+                r -= 1
         return ans
