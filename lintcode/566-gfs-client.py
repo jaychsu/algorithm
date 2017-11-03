f986c90b1916d71270b88715bb40ab9e67964b8d
Jaych Su <hi@jaych.su>
2017-11-03 23:13:08 +0800

    2nd, gfs-client

--- lintcode/566-gfs-client.py
+++ lintcode/566-gfs-client.py
@@ -14,19 +14,20 @@ class GFSClient(BaseGFSClient):
     """
     def __init__(self, chunkSize):
         BaseGFSClient.__init__(self)
-        self.chunkSize = chunkSize
-        self.chunkNum = {}
+        self.chunk_size = chunkSize
+        self.chunk_num = {}
 
     """
     @param: filename: a file name
     @return: conetent of the file given from GFS
     """
     def read(self, filename):
-        if filename not in self.chunkNum:
+        if filename not in self.chunk_num:
             return
-        content = ''
-        for i in range(self.chunkNum[filename]):
-            content += BaseGFSClient.readChunk(self, filename, i)
+        i, content = 0, ''
+        while i < self.chunk_num[filename]:
+            content += self.readChunk(filename, i)
+            i += 1
         return content
 
     """
@@ -35,9 +36,9 @@ class GFSClient(BaseGFSClient):
     @return: nothing
     """
     def write(self, filename, content):
-        i, j, length = 0, 0, len(content)
-        while j < length:
-            BaseGFSClient.writeChunk(self, filename, i, content[j : j + self.chunkSize])
+        i, j, n = 0, 0, len(content)
+        while j < n:
+            self.writeChunk(filename, i, content[j : j + self.chunk_size])
             i += 1
-            j += self.chunkSize
-        self.chunkNum[filename] = i
+            j += self.chunk_size
+        self.chunk_num[filename] = i
