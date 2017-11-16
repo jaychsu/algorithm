7f989049f1e68af6fe3652f52ddaa01ab8885da9
Jaych Su <hi@jaych.su>
2017-11-16 22:19:03 +0800

    1st, maximum-depth-of-binary-tree

---
+++ lintcode/97-maximum-depth-of-binary-tree.py
@@ -0,0 +1,16 @@
+"""
+Definition of TreeNode:
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.left, self.right = None, None
+"""
+class Solution:
+    """
+    @param root: The root of binary tree.
+    @return: An integer
+    """
+    def maxDepth(self, root):
+        if not root:
+            return 0
+        return max(self.maxDepth(root.left), self.maxDepth(root.right)) + 1
