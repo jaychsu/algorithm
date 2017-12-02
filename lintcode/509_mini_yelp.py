a7c009e8253f8630483e598df2690d7ba96f89b9
Jaych Su <hi@jaych.su>
2017-12-03 00:26:15 +0800

    1st, mini_yelp

---
+++ lintcode/509_mini_yelp.py
@@ -0,0 +1,120 @@
+"""
+Definition of Location:
+class Location:
+    # @param {double} latitude, longitude
+    # @param {Location}
+    @classmethod
+    def create(cls, latitude, longitude):
+        # This will create a new location object
+
+Definition of Restaurant:
+class Restaurant:
+    # @param {str} name
+    # @param {Location} location
+    # @return {Restaurant}
+    @classmethod
+    def create(cls, name, location):
+        # This will create a new restaurant object,
+        # and auto fill id
+
+Definition of Helper
+class Helper:
+    # @param {Location} location1, location2
+    @classmethod
+    def get_distance(cls, location1, location2):
+        # return calculate the distance between two location
+
+Definition of GeoHash
+class GeoHash:
+    # @param {Location} location
+    # @return a string
+    @classmethom
+    def encode(cls, location):
+        # return convert location to a geohash string
+
+    # @param {str} hashcode
+    # @return {Location}
+    @classmethod
+    def decode(cls, hashcode):
+        # return convert a geohash string to location
+"""
+import bisect
+from YelpHelper import Location, Restaurant, GeoHash, Helper
+
+
+class MiniYelp:
+    ERROR_IN_KM = (
+        2500, 630, 78,
+        20, 2.4, 0.61,
+        0.076, 0.01911, 0.00478,
+        0.0005971, 0.0001492, 0.0000186
+    )
+
+    restaurants = {}
+    restr_to_geohash = {}
+    geohashs = []
+
+    # @param {str} name
+    # @param {Location} location
+    # @return {int} restaurant's id
+    def add_restaurant(self, name, location):
+        restaurant = Restaurant.create(name, location)
+        hashcode = self.get_restr_hashcode(restaurant)
+
+        self.restaurants[hashcode] = restaurant
+        self.restr_to_geohash[restaurant.id] = hashcode
+        bisect.insort(self.geohashs, hashcode)
+
+        return restaurant.id
+
+    # @param {int} restaurant_id
+    # @return nothing
+    def remove_restaurant(self, restaurant_id):
+        hashcode = self.restr_to_geohash[restaurant_id]
+        index = bisect.bisect_left(self.geohashs, hashcode)
+
+        self.geohashs.pop(index)
+        del self.restaurants[hashcode]
+        del self.restr_to_geohash[restaurant_id]
+
+    # @param {Location} location
+    # @param {double} k, distance smaller than k miles
+    # @return {str[]} a list of restaurant's name and sort by
+    # distance from near to far.
+    def neighbors(self, location, k):
+        length = self.get_length(k)
+        prefix = GeoHash.encode(location)[:length]
+
+        # chr(ord('z') + 1) == '{'
+        left = bisect.bisect_left(self.geohashs, prefix)
+        right = bisect.bisect(self.geohashs, prefix + '{')
+
+        neighbors = []
+        hashcode = restaurant = distance = None
+        for i in range(left, right):
+            hashcode = self.geohashs[i]
+            restaurant = self.restaurants[hashcode]
+            distance = Helper.get_distance(location, restaurant.location)
+            if distance <= k:
+                neighbors.append((distance, restaurant))
+
+        neighbors.sort(key=lambda item: item[0])
+        return [
+            restr.name
+            for _, restr in neighbors
+        ]
+
+    def get_length(self, k):
+        n = len(self.ERROR_IN_KM)
+
+        for i in range(n):
+            if k > self.ERROR_IN_KM[i]:
+                return i
+
+        return n
+
+    def get_restr_hashcode(self, restaurant):
+        return '{0}:{1}'.format(
+            GeoHash.encode(restaurant.location),
+            restaurant.id
+        )
