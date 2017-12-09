3930b0a155c7c46b0a5ba212d3e360b72abe196e
Jaych Su <hi@jaych.su>
2017-12-09 09:42:36 +0800

    1st, remove_duplicate_numbers_in_array

---
+++ lintcode/521_remove_duplicate_numbers_in_array.py
@@ -0,0 +1,47 @@
+"""
+time: O(n)
+space: O(n)
+"""
+class Solution:
+    """
+    @param: nums: an array of integers
+    @return: the number of unique integers
+    """
+    def deduplication(self, nums):
+        ans = 0
+        if not nums:
+            return ans
+
+        exists = {}
+        for i in range(len(nums)):
+            if nums[i] not in exists:
+                exists[nums[i]] = True
+                ans += 1
+
+        return ans
+
+
+"""
+time: O(nlogn)
+space: O(1)
+"""
+class Solution:
+    """
+    @param: nums: an array of integers
+    @return: the number of unique integers
+    """
+    def deduplication(self, nums):
+        ans = 0
+        if not nums:
+            return ans
+
+        nums.sort()
+
+        # for `nums[0]`
+        ans = 1
+        for i in range(1, len(nums)):
+            if nums[i - 1] != nums[i]:
+                nums[ans] = nums[i]
+                ans += 1
+
+        return ans
