f5c8248cf09cf7a21f308c334922cbfc840794f8
Jaych Su <hi@jaych.su>
2017-12-13 00:14:26 +0800

    1st, lfu_cache

--- lintcode/24_lfu_cache.py
+++ lintcode/24_lfu_cache.py
@@ -1,56 +1,192 @@
-from collections import OrderedDict
+"""
+Main Concept:
+
+head <-> 2 <-> 3 <-> 8   |<- freq_list (dll)
+         &     &     &   |<- cache_list (dll)
+         a     c     d
+         &           &
+         b           e
+
+1. if cache is updated (set/get)
+    => freq += 1
+    => move to the tail of cache_list
+2. if cache is full
+    => evict the top-left cache first, that is `a` in above diagram
+    => add the new cache to the tail of `1` in cache_list
+"""
+
 
 class LFUCache:
-    """
-    For a cache with capacity k, if the cache is full and need to evict a key in it,
-    the key with:
-    1. lower frequency of use
-    2. earlier time to save to cache
-    will be kicked out.
-
-    this solution is not good, since the time complexity is quite high.
-    """
-
     """
     @param: capacity: An integer
     """
     def __init__(self, capacity):
-        if capacity < 1:
-            return
-        self.capacity = capacity
-        self.freqs = OrderedDict()
         self.cache = {}
+        self.capacity = capacity
+        self.freq_dummy = None
 
     """
     @param: key: An integer
-    @param: value: An integer
+    @param: val: An integer
     @return: nothing
     """
-    def set(self, key, value):
-        if key in self.cache:
-            self.cache[key] = value
-            self.freqs[key] += 1
+    def set(self, key, val):
+        if self.capacity <= 0:
             return
+
+        if key in self.cache:
+            self._update_item(key, val)
+            return
+
         if len(self.cache) >= self.capacity:
-            lowest_freq = min(self.freqs.values())
-            results = [item for item in self.freqs.items() if item[1] <= lowest_freq]
-            target_key = results[0][0]
-            del self.cache[target_key]
-            del self.freqs[target_key]
-        self.cache[key] = value
-        self.freqs[key] = 0
+            self._evict_item()
+
+        self._add_item(key, val)
 
     """
     @param: key: An integer
     @return: An integer
     """
     def get(self, key):
-        if key in self.cache:
-            freq = self.freqs[key] + 1
-            del self.freqs[key]
-            self.freqs[key] = freq
-            # # python 3
-            # self.freqs[key] += 1
-            # self.freqs.move_to_end(key)
-            return self.cache[key]
-        return -1
+        if key not in self.cache:
+            return -1
+
+        self._update_item(key)
+        return self.cache[key].val
+
+    def _add_item(self, key, val):
+        cache_node = CacheNode(key, val)
+        self.cache[key] = cache_node
+
+        if not self.freq_dummy:
+            self.freq_dummy = FreqNode(0)
+            self.freq_dummy.append_tail(cache_node)
+            return
+
+        if self.freq_dummy.freq == 0:
+            self.freq_dummy.append_tail(cache_node)
+            return
+
+        freq_node = FreqNode(0)
+        freq_node.append_tail(cache_node)
+        self.freq_dummy.before(freq_node)
+        self.freq_dummy = freq_node
+
+    def _evict_item(self):
+        freq_dummy = self.freq_dummy
+        key = freq_dummy.cache_head.key
+        self.cache.pop(key)
+        freq_dummy.pop_head()
+
+        if freq_dummy.is_empty():
+            self.freq_dummy = freq_dummy.nxt
+            freq_dummy.unlink()
+
+    def _update_item(self, key, val=None):
+        cache_node = self.cache[key]
+        freq_node = cache_node.cache_dummy
+        target_freq_node = None
+
+        if val:
+            cache_node.val = val
+
+        if (not freq_node.nxt or
+            freq_node.nxt.freq != freq_node.freq + 1):
+            target_freq_node = FreqNode(freq_node.freq + 1)
+            freq_node.after(target_freq_node)
+        else:
+            target_freq_node = freq_node.nxt
+
+        cache_node.unlink()
+        target_freq_node.append_tail(cache_node)
+
+        if freq_node.is_empty():
+            if self.freq_dummy is freq_node:
+                self.freq_dummy = target_freq_node
+            freq_node.unlink()
+
+
+class CacheNode:
+    def __init__(self, key, val, cache_dummy=None, pre=None, nxt=None):
+        self.key = key
+        self.val = val
+        self.cache_dummy = cache_dummy
+        self.pre = pre
+        self.nxt = nxt
+
+    def unlink(self):
+        if self.cache_dummy.cache_head is self.cache_dummy.cache_tail:
+            self.cache_dummy.cache_head = self.cache_dummy.cache_tail = None
+        elif self.cache_dummy.cache_head is self:
+            self.cache_dummy.cache_head = self.nxt
+            self.nxt.pre = None
+        elif self.cache_dummy.cache_tail is self:
+            self.pre.nxt = None
+            self.cache_dummy.cache_tail = self.pre
+        else:
+            self.pre.nxt = self.nxt
+            self.nxt.pre = self.pre
+
+        self.pre = self.nxt = self.cache_dummy = None
+
+
+class FreqNode:
+    def __init__(self, freq, pre=None, nxt=None):
+        self.freq = freq
+        self.pre = pre
+        self.nxt = nxt
+        self.cache_head = None
+        self.cache_tail = None
+
+    def is_empty(self):
+        return not self.cache_head and not self.cache_tail
+
+    def unlink(self):
+        if self.pre:
+            self.pre.nxt = self.nxt
+        if self.nxt:
+            self.nxt.pre = self.pre
+
+        self.pre = self.nxt = self.cache_head = self.cache_tail = None
+
+    def pop_head(self):
+        if self.is_empty():
+            return
+
+        if self.cache_head is self.cache_tail:
+            cache_node = self.cache_head
+            self.cache_head = self.cache_tail = None
+            return cache_node
+
+        cache_node = self.cache_head
+        self.cache_head = cache_node.nxt
+        cache_node.nxt.pre = None
+        return cache_node
+
+    def append_tail(self, cache_node):
+        cache_node.cache_dummy = self
+
+        if self.is_empty():
+            self.cache_head = self.cache_tail = cache_node
+            return
+
+        cache_node.pre = self.cache_tail
+        cache_node.nxt = None
+        self.cache_tail.nxt = cache_node
+        self.cache_tail = cache_node
+
+    def before(self, freq_node):
+        if self.pre:
+            self.pre.nxt = freq_node
+
+        freq_node.pre = self.pre
+        freq_node.nxt = self
+        self.pre = freq_node
+
+    def after(self, freq_node):
+        if self.nxt:
+            self.nxt.pre = freq_node
+
+        freq_node.pre = self
+        freq_node.nxt = self.nxt
+        self.nxt = freq_node
--- shared/doubly_linked_list.py
+++ shared/doubly_linked_list.py
@@ -1,1 +1,43 @@
-# TODO
+class DoublyLinkedList:
+    def __init__(self, val=None):
+        head = []
+        head[:] = [head, head, val]
+        self.head = head
+
+    def append(self, val):
+        tail = self.head[0]
+        node = [tail, self.head, val]
+        tail[1] = self.head[0] = node
+        return node
+
+    def remove(self, node):
+        pre, nxt, _ = node
+        pre[1] = nxt
+        nxt[0] = pre
+        return node
+
+    def get_tail(self):
+        return self.head[0]
+
+    def sort(self):
+        # TODO
+        pass
+
+    def __repr__(self):
+        return repr(self.head)
+
+    def __iter__(self):
+        curr = self.head
+        while curr:
+            yield curr[2]
+
+            if curr[1] is self.head:
+                break
+
+            curr = curr[1]
+
+    def __getitem__(self, key):
+        return self.head[key]
+
+    def __setitem__(self, key, val):
+        self.head[key] = val
