f991315e283b086afea3e3b312d9ced7797b5cb2
Jaych Su <hi@jaych.su>
2017-12-14 05:07:25 +0800

    Refactoring lru cache with dummy-tail and cyclic DLL

--- lintcode/134_lru_cache.py
+++ lintcode/134_lru_cache.py
@@ -12,6 +12,9 @@ dummy <-> a <-> b <-> c  |<- cache_list (dll)
 """
 
 
+"""
+Dummy Tail Doubly Linked List
+"""
 class LRUCache:
     """
     @param: capacity: An integer
@@ -19,7 +22,7 @@ class LRUCache:
     def __init__(self, capacity):
         self.cache = {}
         self.capacity = capacity
-        self.dummy = self.tail = CacheNode(-1, -1)
+        self.recent = DoublyLinkedList()
 
     """
     @param: key: An integer
@@ -53,14 +56,11 @@ class LRUCache:
     def _add_item(self, key, val):
         cache_node = CacheNode(key, val)
         self.cache[key] = cache_node
-
-        self._append_tail(cache_node)
+        self.recent.append_tail(cache_node)
 
     def _evict_item(self):
-        key = self.dummy.nxt.key
-        self.cache.pop(key)
-
-        self._pop_head()
+        cache_node = self.recent.pop_head()
+        self.cache.pop(cache_node.key)
 
     def _update_item(self, key, val=None):
         cache_node = self.cache[key]
@@ -68,45 +68,200 @@ class LRUCache:
         if val:
             cache_node.val = val
 
-        self._append_tail(cache_node)
-
-    def _pop_head(self):
-        # no nodes
-        if self.dummy is self.tail:
-            return
-
-        # 1 node
-        head = self.dummy.nxt
-        if head is self.tail:
-            self.dummy.nxt = None
-            self.tail = self.dummy
-            return head
-
-        # 2+ nodes
-        head.relink()
-        return head
-
-    def _append_tail(self, cache_node):
-        # passed node is already at end
-        if cache_node is self.tail:
-            return
-
-        cache_node.relink(pre=self.tail)
-        self.tail.nxt = cache_node
-        self.tail = cache_node
+        self.recent.append_tail(cache_node)
 
 
 class CacheNode:
-    def __init__(self, key, val, pre=None, nxt=None):
-        self.key = key
-        self.val = val
+    def __init__(self, key, val=None, pre=None, nxt=None):
         self.pre = pre
         self.nxt = nxt
+        self.key = key
+        self.val = val
 
-    def relink(self, pre=None, nxt=None):
+    def unlink(self):
         if self.pre:
             self.pre.nxt = self.nxt
         if self.nxt:
             self.nxt.pre = self.pre
 
-        self.pre, self.nxt = pre, nxt
+        self.pre = self.nxt = None
+
+
+class DoublyLinkedList:
+    def __init__(self):
+        self.dummy = self.tail = CacheNode(-1)
+
+    def get_head(self):
+        return self.dummy.nxt
+
+    def get_tail(self):
+        if self.dummy is self.tail:
+            return
+
+        return self.tail
+
+    def pop_head(self):
+        if self.dummy is self.tail:
+            return
+
+        head = self.dummy.nxt
+        if head is self.tail:
+            self.tail = self.dummy
+
+        head.unlink()
+        return head
+
+    def append_tail(self, cache_node):
+        if cache_node is self.tail:
+            return
+
+        cache_node.unlink()
+        cache_node.pre = self.tail
+        self.tail.nxt = cache_node
+        self.tail = cache_node
+
+
+# =========================================
+"""
+Cyclic Doubly Linked List
+"""
+class LRUCache:
+    """
+    @param: capacity: An integer
+    """
+    def __init__(self, capacity):
+        self.cache = {}
+        self.capacity = capacity
+        self.recent = DoublyLinkedList()
+
+    """
+    @param: key: An integer
+    @param: val: An integer
+    @return: nothing
+    """
+    def set(self, key, val):
+        if self.capacity <= 0:
+            return
+
+        if key in self.cache:
+            self._update_item(key, val)
+            return
+
+        if len(self.cache) >= self.capacity:
+            self._evict_item()
+
+        self._add_item(key, val)
+
+    """
+    @param: key: An integer
+    @return: An integer
+    """
+    def get(self, key):
+        if key not in self.cache:
+            return -1
+
+        self._update_item(key)
+        _, _, item = self.cache[key]
+        return item['val']
+
+    def _add_item(self, key, val):
+        node = self.recent.new_node(key=key, val=val)
+        self.recent.append_tail(node)
+        self.cache[key] = node
+
+    def _evict_item(self):
+        _, _, item = self.recent.pop_head()
+        self.cache.pop(item['key'])
+
+    def _update_item(self, key, val=None):
+        node = self.cache[key]
+
+        if val:
+            node[2]['val'] = val
+
+        self.recent.unlink(node)
+        self.recent.append_tail(node)
+
+
+class DoublyLinkedList:
+    def __init__(self, **kw):
+        head, _len = None, 0
+
+        if kw:
+            head = []
+            head[:] = self.new_node(head, head, **kw)
+            _len = 1
+
+        self.head = head
+        self._len = _len
+
+    def new_node(self, pre=None, nxt=None, **kw):
+        return [pre, nxt, kw]
+
+    def pop_head(self):
+        if self._len == 0:
+            return self.head
+
+        if self._len == 1:
+            head = self.head
+            self.head = None
+            self._len = 0
+            return head
+
+        return self.unlink(self.head)
+
+    def append_tail(self, node):
+        if self._len == 0:
+            node[:] = self.new_node(node, node, **node[2])
+            self.head = node
+            self._len = 1
+            return node
+
+        tail, _, _ = head = self.head
+        tail[1] = head[0] = node
+        node[0] = tail
+        node[1] = head
+        self._len += 1
+        return node
+
+    def unlink(self, node):
+        pre, nxt, _ = node
+        pre[1] = nxt
+        nxt[0] = pre
+        node[0] = node[1] = None
+
+        if node is self.head:
+            self.head = nxt
+
+        self._len -= 1
+
+        if self._len == 0:
+            self.head = None
+
+        return node
+
+    def __str__(self):
+        return ' <-> '.join([
+            str(item)
+            for _, _, item in self.__iter__()
+        ])
+
+    def __iter__(self):
+        node = self.head
+        while node:
+            _, nxt, _ = node
+            yield node
+
+            if nxt is self.head:
+                break
+
+            node = nxt
+
+    def __bool__(self):
+        return self._len > 0
+
+    def __repr__(self):
+        return repr(self.head)
+
+    def __len__(self):
+        return self._len
--- shared/linked_list_cyclic.py
+++ shared/linked_list_cyclic.py
@@ -1,6 +1,8 @@
 """
+h                 h
 x --> y --> z --> x
 
+h                 h
 x <-> y <-> z <-> x
 """
 
@@ -11,64 +13,107 @@ class LinkedList:
 
 
 class DoublyLinkedList:
-    def __init__(self, val=None):
-        head = []
-        head[:] = [head, head, val]
+    def __init__(self, **kw):
+        head, _len = None, 0
+
+        if kw:
+            head = []
+            head[:] = self.new_node(head, head, **kw)
+            _len = 1
+
         self.head = head
-        self._len = 1 if val else 0
+        self._len = _len
+
+    def new_node(self, pre=None, nxt=None, **kw):
+        return [pre, nxt, kw]
 
     def pop_head(self):
-        # TODO
-        pass
-
-    def append_tail(self, val):
-        if self._len is 0:
-            self.head[2] = val
-            self._len = 1
+        if self._len == 0:
             return self.head
 
-        tail = self.head[0]
-        node = [tail, self.head, val]
-        tail[1] = self.head[0] = node
+        if self._len == 1:
+            head = self.head
+            self.head = None
+            self._len = 0
+            return head
+
+        return self.unlink(self.head)
+
+    def append_tail(self, node):
+        if self._len == 0:
+            node[:] = self.new_node(node, node, **node[2])
+            self.head = node
+            self._len = 1
+            return node
+
+        tail, _, _ = head = self.head
+        tail[1] = head[0] = node
+        node[0] = tail
+        node[1] = head
         self._len += 1
         return node
 
-    def before(self):
-        # TODO
-        pass
+    def append_before(self, a, b):
+        # c <-> a => c <-> b <-> a
+        # note that, b MUST BE no relation with other
+        # or `self.unlink(b)` outside first
+        c, _, _ = a
 
-    def after(self):
-        # TODO
-        pass
+        c[1] = a[0] = b
+        b[0] = c
+        b[1] = a
+
+        if a is self.head:
+            self.head = b
+
+        self._len += 1
+        return b
 
     def unlink(self, node):
         pre, nxt, _ = node
         pre[1] = nxt
         nxt[0] = pre
+        node[0] = node[1] = None
+
+        if node is self.head:
+            self.head = nxt
+
+        self._len -= 1
+
+        if self._len == 0:
+            self.head = None
+
         return node
 
     def sort(self):
         # TODO
         pass
 
+    def __str__(self):
+        return ' <-> '.join([
+            str(item)
+            for _, _, item in self.__iter__()
+        ])
+
+    def __iter__(self):
+        node = self.head
+        while node:
+            _, nxt, _ = node
+            yield node
+
+            if nxt is self.head:
+                break
+
+            node = nxt
+
+    def __bool__(self):
+        return self._len > 0
+
     def __repr__(self):
         return repr(self.head)
 
-    def __str__(self):
-        return ' <-> '.join([str(val) for val in self.__iter__()])
-
     def __len__(self):
         return self._len
-
-    def __iter__(self):
-        curr = self.head
-        while curr:
-            yield curr[2]
-
-            if curr[1] is self.head:
-                break
-
-            curr = curr[1]
 
     def __getitem__(self, key):
         # TODO
