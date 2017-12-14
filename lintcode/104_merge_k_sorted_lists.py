a65ec207d12b442f0eb3d80d85e2f3e2c91e10bc
Jaych Su <hi@jaych.su>
2017-12-14 21:48:54 +0800

    2nd, merge_k_sorted_lists

--- lintcode/104_merge_k_sorted_lists.py
+++ lintcode/104_merge_k_sorted_lists.py
@@ -1,18 +1,4 @@
 """
-Test Case:
-
-[2->null,null,-1->null]
-
-[null,-1->5->11->null,null,6->10->null]
-
-[-10->-7->-6->0->2->2->3->null,-9->null,0->1->3->3->null,-10->-9->-9->-5->-2->2->3->4->null,-9->null,-8->-6->-5->3->3->null,-10->-10->-5->-2->-1->2->2->null,4->null,-9->-5->-3->0->0->null]
-: if node and node.val -> if node and isinstance(node.val, int)
-: this error caused by kicked the `0` out
-"""
-
-from heapq import heappush, heappop
-
-"""
 Definition of ListNode
 class ListNode(object):
 
@@ -20,27 +6,30 @@ class ListNode(object):
         self.val = val
         self.next = next
 """
+from heapq import heappop, heappush
+
+
 class Solution:
     """
     @param lists: a list of ListNode
     @return: The head of one sorted list.
     """
     def mergeKLists(self, lists):
-        ans = None
         if not lists:
-            return ans
-        parent = ans
-        heap = []
+            return
+
+        dummy = tail = ListNode(-1)
+        candidates = []
         for node in lists:
-            if node and isinstance(node.val, int):
-                heappush(heap, (node.val, node))
-        while heap:
-            _, node = heappop(heap)
-            if parent:
-                parent.next = node
-                parent = parent.next
-            else:
-                ans = parent = node
-            if parent.next:
-                heappush(heap, (parent.next.val, parent.next))
-        return ans
+            if node:
+                heappush(candidates, (node.val, node))
+
+        while candidates:
+            _, node = heappop(candidates)
+            tail.next = node
+            tail = tail.next
+
+            if node.next:
+                heappush(candidates, (node.next.val, node.next))
+
+        return dummy.next
