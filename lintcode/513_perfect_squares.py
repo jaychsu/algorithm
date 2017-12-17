63cd1e239ee64310a8f8d72b4da02846781bf9e0
Jaych Su <hi@jaych.su>
2017-12-17 09:58:55 +0800

    2nd, perfect_squares

--- lintcode/513_perfect_squares.py
+++ lintcode/513_perfect_squares.py
@@ -11,7 +11,7 @@ class Solution:
     @return: An integer
     """
     def numSquares(self, n):
-        if not n:
+        if n <= 0:
             return 0
 
         INFINITY = float('inf')
@@ -22,9 +22,9 @@ class Solution:
         dp[1] = 1
 
         for i in range(1, n + 1):
-            for j in range(1, i):
-                if j * j > i:
-                    break
+            j = 1
+            while j * j <= i:
                 dp[i] = min(dp[i], dp[i - j * j] + 1)
+                j += 1
 
         return dp[n]
