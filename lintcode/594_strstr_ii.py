90945660f5ec17a363daf3bb47c4f19e21aee89a
Jaych Su <hi@jaych.su>
2017-12-20 00:17:58 +0800

    2nd, strstr_ii

--- lintcode/594_strstr_ii.py
+++ lintcode/594_strstr_ii.py
@@ -3,22 +3,23 @@ Rabin-Karp algorithm
 
 https://en.wikipedia.org/wiki/Rabin%E2%80%93Karp_algorithm
 
+
 Main Concept:
 
 given S == 'abcde' and T == 'bcd'
 
 1. convert T into hashcode `tcode`
-2. iterate S every m chars, and compare with `tcode`,
-   continue to add `S[i]` and kick out `S[i - m]`,
+2. iterate S every `n` chars, and compare with `tcode`,
+   continue to add `S[i]` and kick out `S[i - n]`,
 
     a b c d e
     a b c
       b c d
         c d e
 
-3. so we need to reduce `ord(S[i - m]) * MAGIC_NUMBER ** (m - 1)` every turns
-   => calculate `MAGIC_NUMBER ** (m - 1)` in advance to save time
-4. if became negative, add `BASE` to back to positive
+3. so we need to reduce `ord(S[i - n]) * MG ** (n - 1)` every turns
+   => calculate `MG ** (n - 1)` in advance to save time
+4. if became negative, add `MOD` to back to positive
 
 Test Case:
 
@@ -31,53 +32,58 @@ Test Case:
 
 
 class Solution:
-    """
-    @param: S: A source string
-    @param: T: A target string
-    @return: An integer as index
-    """
     def strStr2(self, S, T):
-        NOT_FOUND = -1
-        if S is None or T is None:
-            return NOT_FOUND
+        """
+        :type S: List[str]
+        :type T: List[str]
+        :rtype: int
+        """
 
-        if T is '':
+        NOT_FOUND = -1
+
+        if S is not None and T is '':
             return 0
 
-        n, m = len(S), len(T)
-
-        if m > n:
+        if not S or not T:
             return NOT_FOUND
 
-        BASE = 1000000
-        MAGIC_NUMBER = 31
+        m, n = len(S), len(T)
+        if n > m:
+            return NOT_FOUND
 
+        MOD = 1000000  # hashsize to mod
+        MG = 31  # magic number
+        A = ord('a')
+
+        p = 1  # `p == MG ** (n - 1)`
         tcode = 0  # the code of T
-        top = 1  # top == `MAGIC_NUMBER ** (m - 1)`
-        for i in range(m):
-            # task 1: get `tcode`
-            tcode = (tcode * MAGIC_NUMBER + self._to_int(T[i])) % BASE
+        for i in range(n):
+            tcode = (tcode * MG + ord(T[i]) - A) % MOD
 
-            # task 2: get `MAGIC_NUMBER ** (m - 1)`
-            if i > 0:
-                top = (top * MAGIC_NUMBER) % BASE
+            if i == 0:
+                continue
+            """
+            continue here since p only need `n - 1` times
+            """
+            p = (p * MG) % MOD
 
         _code = 0
-        for i in range(n):
-            # kick out `S[i - m]`
-            if i >= m:
-                _code = (_code - self._to_int(S[i - m]) * top) % BASE
+        for i in range(m):
+            """
+            kick out `S[i - n]`
+            """
+            if i >= n:
+                _code = (_code - (ord(S[i - n]) - A) * p) % MOD
 
             if _code < 0:
-                _code += BASE
+                _code += MOD
 
-            # add `S[i]`
-            _code = (_code * MAGIC_NUMBER + self._to_int(S[i])) % BASE
+            """
+            Add `S[i]`
+            """
+            _code = (_code * MG + ord(S[i]) - A) % MOD
 
-            if _code == tcode and S[i - m + 1:i + 1] == T:
-                return i - m + 1
+            if _code == tcode and S[i - n + 1:i + 1] == T:
+                return i - n + 1
 
         return NOT_FOUND
-
-    def _to_int(self, char):
-        return ord(char) - ord('a')
