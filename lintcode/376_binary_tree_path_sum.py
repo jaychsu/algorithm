6540b266f44b23d7a75d4cfefb2032edc1339745
Jaych Su <hi@jaych.su>
2017-12-22 15:21:12 +0800

    1st, binary_tree_path_sum

---
+++ lintcode/376_binary_tree_path_sum.py
@@ -0,0 +1,32 @@
+"""
+Definition of TreeNode:
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.left, self.right = None, None
+"""
+
+
+class Solution:
+    """
+    @param: root: the root of binary tree
+    @param: target: An integer
+    @return: all valid paths
+    """
+    def binaryTreePathSum(self, root, target):
+        ans = []
+        self.dfs(root, target, ans, [])
+        return ans
+
+    def dfs(self, node, remaining, ans, path):
+        if not node:
+            return
+
+        path += [node.val]
+
+        remaining -= node.val
+        if remaining == 0 and not node.left and not node.right:
+            ans.append(path[:])
+
+        self.dfs(node.left, remaining, ans, path[:])
+        self.dfs(node.right, remaining, ans, path[:])
