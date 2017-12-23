199438ddc6057dfbce3ed1703a3f8175985ca625
Jaych Su <hi@jaych.su>
2017-12-23 17:07:03 +0800

    4th, friendship_service

--- lintcode/560_friendship_service.py
+++ lintcode/560_friendship_service.py
@@ -1,16 +1,19 @@
 class FriendshipService:
+    def __init__(self):
+        self.followers = {}
+        self.followings = {}
 
-    followers = {}
-    followings = {}
+    def getFollowers(self, user_id):
+        return self.get_followers(user_id)
+
+    def getFollowings(self, user_id):
+        return self.get_followings(user_id)
 
     """
     @param: user_id: An integer
     @return: all followers and sort by user_id
     """
     def get_followers(self, user_id):
-        return self.getFollowers(user_id)
-
-    def getFollowers(self, user_id):
         if user_id in self.followers:
             return sorted(self.followers[user_id])
 
@@ -21,35 +24,33 @@ class FriendshipService:
     @return: all followings and sort by user_id
     """
     def get_followings(self, user_id):
-        return self.getFollowings(user_id)
-
-    def getFollowings(self, user_id):
         if user_id in self.followings:
             return sorted(self.followings[user_id])
 
         return []
 
     """
-    @param: to_user_id: An integer
-    @param: from_user_id: An integer
+    @param: to_id: An integer
+    @param: from_id: An integer
     @return: nothing
     """
-    def follow(self, to_user_id, from_user_id):
-        if to_user_id not in self.followers:
-            self.followers[to_user_id] = set()
-        if from_user_id not in self.followings:
-            self.followings[from_user_id] = set()
+    def follow(self, to_id, from_id):
+        if from_id not in self.followings:
+            self.followings[from_id] = set()
+        self.followings[from_id].add(to_id)
 
-        self.followers[to_user_id].add(from_user_id)
-        self.followings[from_user_id].add(to_user_id)
+        if to_id not in self.followers:
+            self.followers[to_id] = set()
+        self.followers[to_id].add(from_id)
 
     """
-    @param: to_user_id: An integer
-    @param: from_user_id: An integer
+    @param: to_id: An integer
+    @param: from_id: An integer
     @return: nothing
     """
-    def unfollow(self, to_user_id, from_user_id):
-        if to_user_id in self.followers:
-            self.followers[to_user_id].discard(from_user_id)
-        if from_user_id in self.followings:
-            self.followings[from_user_id].discard(to_user_id)
+    def unfollow(self, to_id, from_id):
+        if from_id in self.followings:
+            self.followings[from_id].discard(to_id)
+
+        if to_id in self.followers:
+            self.followers[to_id].discard(from_id)
