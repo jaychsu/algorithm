ff3c5bdb0703d7939d520a53701be9fb9844547e
Jaych Su <hi@jaych.su>
2017-12-23 11:38:58 +0800

    1st, convert_binary_search_tree_to_doubly_linked_list

---
+++ lintcode/378_convert_binary_search_tree_to_doubly_linked_list.py
@@ -0,0 +1,43 @@
+"""
+Definition of TreeNode:
+class TreeNode:
+    def __init__(self, val):
+        this.val = val
+        this.left, this.right = None, None
+
+Definition of Doubly-ListNode
+class DoublyListNode(object):
+
+    def __init__(self, val, next=None):
+        self.val = val
+        self.next = self.prev = next
+"""
+
+
+class Solution:
+    """
+    @param: root: The root of tree
+    @return: the head of doubly list node
+    """
+    def bstToDoublyList(self, root):
+        if not root:
+            return
+
+        dummy = tail = DoublyListNode(-1)
+
+        stack = []
+        node = root
+        while node or stack:
+            while node:
+                stack.append(node)
+                node = node.left
+
+            node = stack.pop()
+
+            tail.next = _node = DoublyListNode(node.val)
+            _node.prev = tail
+            tail = tail.next
+
+            node = node.right
+
+        return dummy.next
