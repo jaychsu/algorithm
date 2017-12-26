7332ef771f841d3101544b3129357372c66cdb66
Jaych Su <hi@jaych.su>
2017-12-26 17:03:35 +0800

    1st, intersection_of_two_arrays

---
+++ lintcode/547_intersection_of_two_arrays.py
@@ -0,0 +1,28 @@
+class Solution:
+    """
+    @param: A: an integer array
+    @param: B: an integer array
+    @return: an integer array
+    """
+    def intersection(self, A, B):
+        ans = []
+        if not A or not B:
+            return ans
+
+        A.sort()
+        B.sort()
+
+        i = j = 0
+        while i < len(A) and j < len(B):
+            if A[i] == B[j]:
+                if not ans or ans[-1] != A[i]:
+                    ans.append(A[i])
+                i += 1
+                j += 1
+                continue
+            if A[i] < B[j]:
+                i += 1
+            else:
+                j += 1
+
+        return ans
