57f864e84ae609560974b04c3a72e32ae2177d79
Jaych Su <hi@jaych.su>
2017-12-27 13:50:52 +0800

    2nd, word_ladder

---
+++ leetcode/127_word_ladder.md
@@ -0,0 +1,1 @@
+see [lintcode/120_word_ladder.py](../lintcode/120_word_ladder.py)
--- lintcode/120_word_ladder.py
+++ lintcode/120_word_ladder.py
@@ -9,24 +9,25 @@ Test Case:
 
 
 class Solution:
-    """
-    @param: start: a string
-    @param: end: a string
-    @param: D: a set of string
-    @return: An integer
-    """
-    def ladderLength(self, start, end, D):
-        if (not start or not end or
-            len(start) != len(end) or
-            not D):
+    def ladderLength(self, s, e, D):
+        """
+        :type s: str
+        :type e: str
+        :type D: List[str]
+        :rtype: int
+        """
+        if (not s or not e or
+            len(s) != len(e) or not D):
             return 0
+        if s == e:
+            return 1
 
-        if start not in D:
-            D.add(start)
-        if end not in D:
-            D.add(end)
+        if s not in D:
+            D.append(s)
+        if e not in D:
+            D.append(e)
 
-        n = len(start)
+        n = len(s)
         next_words = [None] * n
         for i in range(n):
             next_words[i] = _words = {}
@@ -36,15 +37,15 @@ class Solution:
                     _words[key] = set()
                 _words[key].add(word)
 
-        distance = {end: 1}
-        queue = [end]
+        queue = [e]
+        distance = {e: 1}
         for word in queue:
-            if word == start:
-                return distance[word]
             for _word in self.get_next_word(word, next_words):
                 if _word in distance:
                     continue
                 distance[_word] = distance[word] + 1
+                if _word == s:
+                    return distance[_word]
                 queue.append(_word)
 
         return 0
