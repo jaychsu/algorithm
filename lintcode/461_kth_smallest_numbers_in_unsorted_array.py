ba57300442e5afe68a3de535d12dde78139caeff
Jaych Su <hi@jaych.su>
2017-12-29 13:21:06 +0800

    3rd, kth_smallest_numbers_in_unsorted_array

--- lintcode/461_kth_smallest_numbers_in_unsorted_array.py
+++ lintcode/461_kth_smallest_numbers_in_unsorted_array.py
@@ -1,9 +1,36 @@
 class Solution:
     """
-    @param: k: An integer
-    @param: nums: An integer array
+    @param: K: An integer
+    @param: A: An integer array
     @return: kth smallest element
     """
-    def kthSmallest(self, k, nums):
-        nums.sort()
-        return nums[k-1]
+    def kthSmallest(self, K, A):
+        """
+        the index of `K`th child is `K - 1`
+        """
+        return self.quick_select(K - 1, A, 0, len(A) - 1)
+
+    def quick_select(self, k, A, start, end):
+        if start >= end:
+            return A[end]
+
+        left, right = start, end
+        pivot = A[(start + end) // 2]
+
+        while left <= right:
+            while left <= right and A[left] < pivot:
+                left += 1
+            while left <= right and A[right] > pivot:
+                right -= 1
+
+            if left <= right:
+                A[left], A[right] = A[right], A[left]
+                left += 1
+                right -= 1
+
+        if start <= k <= right:
+            return self.quick_select(k, A, start, right)
+        elif left <= k <= end:
+            return self.quick_select(k, A, left, end)
+        else:
+            return A[k]
