88312c0dadeefc0d19210c59756ba17bcbb9edb8
Jaych Su <hi@jaych.su>
2017-12-29 17:35:23 +0800

    2nd, top_k_largest_numbers_ii

--- lintcode/545_top_k_largest_numbers_ii.py
+++ lintcode/545_top_k_largest_numbers_ii.py
@@ -7,20 +7,22 @@ class Solution:
     """
     def __init__(self, k):
         self.k = k
-        self.top_k = []
+        self.tops = []
 
     """
     @param: num: Number to be added
     @return: nothing
     """
     def add(self, num):
-        heappush(self.top_k, num)
+        # push in first, since we cannot ensure
+        # the incoming num will stay in tops
+        heappush(self.tops, num)
 
-        if len(self.top_k) > self.k:
-            heappop(self.top_k)
+        if len(self.tops) > self.k:
+            heappop(self.tops)
 
     """
     @return: Top k element
     """
     def topk(self):
-        return sorted(self.top_k, reverse=True)
+        return sorted(self.tops, reverse=True)
