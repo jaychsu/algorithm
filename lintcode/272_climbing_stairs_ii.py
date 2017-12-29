fabcb85d9278c1abef1a5fbbb8ce3e32a27d384b
Jaych Su <hi@jaych.su>
2017-12-30 03:49:44 +0800

    1st, climbing_stairs_ii

---
+++ lintcode/272_climbing_stairs_ii.py
@@ -0,0 +1,42 @@
+"""
+dp: space optimized by rolling array
+"""
+class Solution:
+    """
+    @param: n: An integer
+    @return: An integer
+    """
+    def climbStairs2(self, n):
+        if not n or n <= 1:
+            return 1
+        if n == 2:
+            return 2
+
+        a, b, c = 1, 1, 2
+        for i in range(3, n + 1):
+            a, b, c = b, c, a + b + c
+
+        return c
+
+
+"""
+dp
+"""
+class Solution:
+    """
+    @param: n: An integer
+    @return: An integer
+    """
+    def climbStairs2(self, n):
+        if not n:
+            return 0
+
+        dp = [0] * (n + 1)
+        dp[0] = 1
+        dp[1] = 1
+        dp[2] = 2
+
+        for i in range(3, n + 1):
+            dp[i] = dp[i - 3] + dp[i - 2] + dp[i - 1]
+
+        return dp[n]
