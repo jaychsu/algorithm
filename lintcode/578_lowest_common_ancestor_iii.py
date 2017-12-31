167c675dcc510dade034e3e2f310022894e485db
Jaych Su <hi@jaych.su>
2017-12-31 20:27:04 +0800

    2nd, lowest_common_ancestor_iii

--- lintcode/578_lowest_common_ancestor_iii.py
+++ lintcode/578_lowest_common_ancestor_iii.py
@@ -4,8 +4,8 @@ class TreeNode:
     def __init__(self, val):
         this.val = val
         this.left, this.right = None, None
-"""
-"""
+
+
 Test Case:
 
 {1,#,2,#,3,#,4,#,5}
@@ -17,6 +17,7 @@ Test Case:
 1
 """
 
+
 class Solution:
     """
     @param: root: The root of the binary tree.
@@ -24,31 +25,39 @@ class Solution:
     @param: B: A TreeNode
     @return: Return the LCA of the two nodes.
     """
-    """
-    for each node in tree, we can record whether A or B exists in its subtree
-    use post-traversal to make a bottom-up searching
-    """
     def lowestCommonAncestor3(self, root, A, B):
-        a_exist, b_exist, lca = self._divide_conquer(root, A, B)
-        if a_exist and b_exist:
+        """
+        for each node in tree, we can record whether A or B exists in its subtree
+        use post-traversal to make a bottom-up searching
+
+        node A or node B may not exist in tree.
+        so we can't directly use divide conquer like lintcode/88_lowest_common_ancestor.py
+        => {4,3,7,#,#,5,6}, 1, 3
+        => expected `None`
+        => got `3` if using divide conquer
+        """
+        lca, has_a, has_b = self.divide_conquer(root, A, B)
+
+        if has_a and has_b:
             return lca
 
-    def _divide_conquer(self, node, A, B):
+    def divide_conquer(self, node, A, B):
         if not node:
-            return False, False, None
+            return None, False, False
 
-        a_exist_in_left,  b_exist_in_left,  left_child  = self._divide_conquer(node.left,  A, B)
-        a_exist_in_right, b_exist_in_right, right_child = self._divide_conquer(node.right, A, B)
+        left,  a_in_left,  b_in_left = self.divide_conquer(node.left, A, B)
+        right, a_in_right, b_in_right = self.divide_conquer(node.right, A, B)
 
-        a_exist = a_exist_in_left or a_exist_in_right or node == A
-        b_exist = b_exist_in_left or b_exist_in_right or node == B
+        has_a = a_in_left or a_in_right or node is A
+        has_b = b_in_left or b_in_right or node is B
 
-        if node == A or node == B:
-            return a_exist, b_exist, node
-        if left_child and right_child:
-            return a_exist, b_exist, node
-        if left_child:
-            return a_exist, b_exist, left_child
-        if right_child:
-            return a_exist, b_exist, right_child
-        return a_exist, b_exist, None
+        if node is A or node is B:
+            return node, has_a, has_b
+        if left and right:
+            return node, has_a, has_b
+        if left:
+            return left, has_a, has_b
+        if right:
+            return right, has_a, has_b
+
+        return None, has_a, has_b
