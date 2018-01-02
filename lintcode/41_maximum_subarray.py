d4f934d8c736c19658255120cc7b03ae5f31bcb7
Jaych Su <hi@jaych.su>
2018-01-02 22:14:45 +0800

    2nd, maximum_subarray

--- lintcode/41_maximum_subarray.py
+++ lintcode/41_maximum_subarray.py
@@ -1,16 +1,69 @@
+"""
+Prefix Sum: space optimization
+"""
 class Solution:
     """
-    @param: nums: A list of integers
+    @param: A: A list of integers
     @return: A integer indicate the sum of max subarray
     """
-    def maxSubArray(self, nums):
-        if not nums:
+    def maxSubArray(self, A):
+        """
+        the answer is the maximum segment sum,
+        that is, `S[i] - Smin`
+        """
+        if not A:
             return 0
 
-        max_sum, min_sum, prefix_sum = float('-inf'), 0, 0
-        for i in range(len(nums)):
-            prefix_sum += nums[i]
-            max_sum = max(max_sum, prefix_sum - min_sum)
-            min_sum = min(min_sum, prefix_sum)
+        ans = A[0]
+        S = Smin = 0
+        for i in range(len(A)):
+            S += A[i]
 
-        return max_sum
+            if S - Smin > ans:
+                ans = S - Smin
+
+            """
+            since the sum of [i, j] in A is `S[j] - S[i - 1]`
+            so we need to find the `Smin` at the end of iteration
+            """
+            if S < Smin:
+                Smin = S
+
+        return ans
+
+
+"""
+Prefix Sum
+"""
+class Solution:
+    """
+    @param: A: A list of integers
+    @return: A integer indicate the sum of max subarray
+    """
+    def maxSubArray(self, A):
+        """
+        the answer is the maximum segment sum,
+        that is, `S[i] - Smin`
+        """
+        if not A:
+            return
+
+        n = len(A)
+        ans = A[0]
+
+        Smin = 0
+        S = [0] * (n + 1)
+        for i in range(1, n + 1):
+            S[i] = S[i - 1] + A[i - 1]
+
+            if S[i] - Smin > ans:
+                ans = S[i] - Smin
+
+            """
+            since the sum of [i, j] in A is `S[j] - S[i - 1]`
+            so we need to find the `Smin` at the end of iteration
+            """
+            if S[i] < Smin:
+                Smin = S[i]
+
+        return ans
