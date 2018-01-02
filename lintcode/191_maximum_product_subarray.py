0b7400eae97d29e433047239897a247ea7d84ebb
Jaych Su <hi@jaych.su>
2018-01-03 00:35:41 +0800

    2nd, maximum_product_subarray

--- lintcode/191_maximum_product_subarray.py
+++ lintcode/191_maximum_product_subarray.py
@@ -1,22 +1,30 @@
+"""
+since the fact:
+    the minimum negative number * -1 -> the maximum
+    the maximum positive number -> the maximum
+so we need record the minimum and the maximum number for each child in nums
+"""
+
+
 class Solution:
     """
-    @param: nums: An array of integers
+    @param: A: An array of integers
     @return: An integer
     """
-    """
-    since the fact:
-        the minimum negative number * -1 -> the maximum
-        the maximum positive number -> the maximum
-    so we need record the minimum and the maximum number for each child in nums
-    """
-    def maxProduct(self, nums):
-        if not nums:
+    def maxProduct(self, A):
+        if not A:
             return 0
 
-        ans = min_p = max_p = nums[0]
-        for i in range(1, len(nums)):
-            min_p, max_p = \
-                min(nums[i], min_p * nums[i], max_p * nums[i]), \
-                max(nums[i], min_p * nums[i], max_p * nums[i])
-            ans = max(ans, max_p)
+        ans = Pmin = Pmax = A[0]
+        for i in range(1, len(A)):
+            """
+            adding `A[i]` to reset `min` and `max`
+            if its so lowest or highest
+            """
+            C = (A[i], Pmin * A[i], Pmax * A[i])
+            Pmin, Pmax = min(C), max(C)
+
+            if Pmax > ans:
+                ans = Pmax
+
         return ans
