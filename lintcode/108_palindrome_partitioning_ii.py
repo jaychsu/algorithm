06676f89e5015e567f088715beadf4f50888d4d9
Jaych Su <hi@jaych.su>
2018-01-02 17:19:02 +0800

    2nd, palindrome_partitioning_ii

--- lintcode/108_palindrome_partitioning_ii.py
+++ lintcode/108_palindrome_partitioning_ii.py
@@ -1,48 +1,46 @@
 class Solution:
-    is_palindrome = None
-
-    # @param s, a string
+    # @param S, a string
     # @return an integer
-    def minCut(self, s):
-        if not s:
-            return 0
+    def minCut(self, S):
+        if not S:
+            return -1
 
-        n = len(s)
+        n = len(S)
         INFINITY = float('inf')
+        is_palindrome = self.get_palin_map(S)
 
-        self.check_palindrome(s)
-
-        # `dp[i]` means the palindrome partitioning of `s[0:i-1]`
+        """
+        `dp[i]` means the minimum palindrome count
+        broken from the substring ended at `i`
+        """
         dp = [INFINITY] * (n + 1)
         dp[0] = 0
 
         for end in range(1, n + 1):
             for start in range(end):
-                if (not self.is_palindrome[start][end - 1]
-                    or dp[start] is INFINITY):
+                if (not is_palindrome[start][end - 1] or
+                    dp[start] is INFINITY):
                     continue
-                dp[end] = min(dp[end], dp[start] + 1)
+                if dp[start] + 1 < dp[end]:
+                    dp[end] = dp[start] + 1
 
-        # since `cuts = partitions - 1`
         return dp[n] - 1
 
-    def check_palindrome(self, s):
-        n = len(s)
-        self.is_palindrome = [[False] * n for _ in range(n)]
-        start = end = 0
+    def get_palin_map(self, S):
+        n = len(S)
+        is_palindrome = [[False] * n for _ in range(n)]
+        is_palindrome[0][0] = True
 
-        for end in range(n):
-            self.is_palindrome[end][end] = True
+        for end in range(1, n):
+            is_palindrome[end][end] = True
 
-            if end > 0:
-                start = end - 1
-                self.is_palindrome[start][end] = (
-                    s[start] == s[end]
-                )
+            start = end - 1
+            is_palindrome[start][end] = (S[start] == S[end])
 
         for start in range(n - 1 - 2, -1, -1):
             for end in range(start + 2, n):
-                self.is_palindrome[start][end] = (
-                    self.is_palindrome[start + 1][end - 1]
-                    and s[start] == s[end]
-                )
+                if not is_palindrome[start + 1][end - 1]:
+                    continue
+                is_palindrome[start][end] = (S[start] == S[end])
+
+        return is_palindrome
