6918fb0dd235252da5c4714b1fa2aa3d60af5cc5
Jaych Su <hi@jaych.su>
2018-01-05 19:33:00 +0800

    2nd, search_in_rotated_sorted_array

--- lintcode/62_search_in_rotated_sorted_array.py
+++ lintcode/62_search_in_rotated_sorted_array.py
@@ -1,48 +1,50 @@
+"""
+for given A:
+    [ 4, 5, 6, 7, 0, 1, 2 ]
+      l        M  m     r
+
+if the `mid` > `l`, then `mid` must be in left side, otherwise in right side
+
+if `mid` in left side, we can check if the `target` between `l` and `mid`
+(because we dont know the `M` at where)
+
+otherwise the `mid` in right side, we can also check if the `target` between `mid` and `r`
+(same reason, we dont know the `m` at where)
+"""
+
+
 class Solution:
     """
     @param: A: an integer rotated sorted array
     @param: target: an integer to be searched
     @return: an integer
     """
-    """
-    for a given A:
-        [ 4, 5, 6, 7, 0, 1, 2 ]
-          l        M  m     r
-
-    if the `mid` > `l`, then `mid` must be in left side, otherwise in right side
-
-    if `mid` in left side, we can check if the `target` between `l` and `mid`
-    (because we dont know the `M` at where)
-
-    otherwise the `mid` in right side, we can also check if the `target` between `mid` and `r`
-    (same reason, we dont know the `m` at where)
-    """
     def search(self, A, target):
+        NOT_FOUND = -1
         if not A:
-            return -1
+            return NOT_FOUND
 
-        l, m, r = 0, 0, len(A) - 1
-        while l + 1 < r:
-            m = l + (r - l) // 2
-            if A[m] == target:
-                return m
+        left, right = 0, len(A) - 1
+        while left + 1 < right:
+            mid = (left + right) // 2
 
-            # in the left side in array
-            if A[m] > A[l]:
-                if A[l] <= target < A[m]:
-                    r = m
+            if A[mid] == target:
+                return mid
+
+            if A[mid] < A[0]:
+                if A[mid] < target <= A[right]:
+                    left = mid
                 else:
-                    l = m
-
-            # in the right side in array
+                    right = mid
             else:
-                if A[m] < target <= A[r]:
-                    l = m
+                if A[left] <= target < A[mid]:
+                    right = mid
                 else:
-                    r = m
+                    left = mid
 
-        for end in [l, r]:
-            if A[end] == target:
-                return end
+        if A[left] == target:
+            return left
+        if A[right] == target:
+            return right
 
-        return -1
+        return NOT_FOUND
