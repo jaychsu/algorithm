c5dac443f33f38ca14aab2f0aa9b9da2e1e9641c
Jaych Su <hi@jaych.su>
2018-01-05 19:44:21 +0800

    2nd, sort_colors

--- lintcode/148_sort_colors.py
+++ lintcode/148_sort_colors.py
@@ -1,23 +1,28 @@
 class Solution:
     """
-    @param: nums: A list of integer which is 0, 1 or 2
+    @param: A: A list of integer which is 0, 1 or 2
     @return: nothing
     """
-    def sortColors(self, nums):
-        if not nums:
+    def sortColors(self, A):
+        if not A:
             return
 
-        left, i, right = 0, 0, len(nums) - 1
+        left, right = 0, len(A) - 1
+        i = 0
         while i <= right:
-            if nums[i] == 0:
-                nums[left], nums[i] = nums[i], nums[left]
+            if A[i] == 0:
+                A[left], A[i] = A[i], A[left]
                 left += 1
                 i += 1
-            elif nums[i] == 1:
-                # temply ignore it
-                # it will be swapped if there is `0` later
+            elif A[i] == 1:
+                """
+                temply ignore it
+                it will be swapped if there is `0` later
+                """
                 i += 1
             else:
-                # cannot `i += 1` since the swapped value still need to check
-                nums[i], nums[right] = nums[right], nums[i]
+                """
+                cannot `i += 1` since the swapped value still need to check
+                """
+                A[right], A[i] = A[i], A[right]
                 right -= 1
