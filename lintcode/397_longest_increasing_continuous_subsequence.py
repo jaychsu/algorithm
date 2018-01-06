feb455cb189dfc36f37f2c608efde7202c34d633
Jaych Su <hi@jaych.su>
2018-01-06 20:54:38 +0800

    2nd, longest_increasing_continuous_subsequence

--- lintcode/397_longest_increasing_continuous_subsequence.py
+++ lintcode/397_longest_increasing_continuous_subsequence.py
@@ -11,44 +11,40 @@ class Solution:
         if not A:
             return 0
 
-        return max(
-            self.get_lics_size(A),
-            self.get_lics_size(list(reversed(A)))
-        )
+        size = self.get_lics_size(A)
+        A.reverse()
+        _size = self.get_lics_size(A)
+
+        return max(size, _size)
 
     def get_lics_size(self, A):
-        lics_size = 0
+        ans = 0
         n = len(A)
 
         """
-        `F[i]` means the size of LICS end at `F[i]`
+        `dp[i]` means the size of LICS ended at `A[i]`
         note that there is size, so init with `1`
         """
-        F = [1] * n
+        dp = [1] * n
 
-        """
-        path
-        `path_start` means the previous index if `A[i]` is included in the LICS
-        `path_end` means the path end index
-        """
-        # path_start = [-1] * n
-        # path_end = 0
+        # pi = [-1] * n
+        # end_at = -1
 
         for i in range(n):
             if i > 0 and A[i] > A[i - 1]:
-                F[i] = F[i - 1] + 1
-                # path_start[i] = i - 1
-            if F[i] > lics_size:
-                lics_size = F[i]
-                # path_end = i
+                dp[i] = dp[i - 1] + 1
+                # pi[i] = i - 1
+            if dp[i] > ans:
+                ans = dp[i]
+                # end_at = i
 
-        # paths = [0] * lics_size
-        # for i in range(lics_size - 1, -1, -1):
-        #     paths[i] = A[path_end]
-        #     path_end = path_start[path_end]
+        # paths = [0] * ans
+        # for i in range(ans - 1, -1, -1):
+        #     paths[i] = A[end_at]
+        #     end_at = pi[end_at]
         # print(paths)
 
-        return lics_size
+        return ans
 
 
 """
@@ -63,20 +59,22 @@ class Solution:
         if not A:
             return 0
 
-        return max(
-            self.get_lics_size(A),
-            self.get_lics_size(list(reversed(A)))
-        )
+        size = self.get_lics_size(A)
+        A.reverse()
+        _size = self.get_lics_size(A)
+
+        return max(size, _size)
 
     def get_lics_size(self, A):
-        lics_size = size = 1
+        ans = size = 1
 
-        for i in range(len(A)):
-            if i > 0 and A[i] > A[i - 1]:
+        for i in range(1, len(A)):
+            if A[i] > A[i - 1]:
                 size += 1
             else:
                 size = 1
-            if size > lics_size:
-                lics_size = size
 
-        return lics_size
+            if size > ans:
+                ans = size
+
+        return ans
