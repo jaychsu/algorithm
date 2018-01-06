4c5f07c9b2ef1aa38dbbf6dba28643c4a0577f53
Jaych Su <hi@jaych.su>
2018-01-06 20:00:52 +0800

    2nd, convert_binary_search_tree_to_doubly_linked_list

--- lintcode/378_convert_binary_search_tree_to_doubly_linked_list.py
+++ lintcode/378_convert_binary_search_tree_to_doubly_linked_list.py
@@ -7,7 +7,6 @@ class TreeNode:
 
 Definition of Doubly-ListNode
 class DoublyListNode(object):
-
     def __init__(self, val, next=None):
         self.val = val
         self.next = self.prev = next
