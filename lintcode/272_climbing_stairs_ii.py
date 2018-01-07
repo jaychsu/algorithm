9772a230e0906c404380e5c1344e3a04191397e5
Jaych Su <hi@jaych.su>
2018-01-07 18:13:37 +0800

    2nd, climbing_stairs_ii

--- lintcode/272_climbing_stairs_ii.py
+++ lintcode/272_climbing_stairs_ii.py
@@ -28,15 +28,16 @@ class Solution:
     @return: An integer
     """
     def climbStairs2(self, n):
-        if not n:
-            return 0
+        if not n or n <= 1:
+            return 1
+        if n == 2:
+            return 2
 
         dp = [0] * (n + 1)
-        dp[0] = 1
-        dp[1] = 1
+        dp[0] = dp[1] = 1
         dp[2] = 2
 
         for i in range(3, n + 1):
-            dp[i] = dp[i - 3] + dp[i - 2] + dp[i - 1]
+            dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3]
 
         return dp[n]
