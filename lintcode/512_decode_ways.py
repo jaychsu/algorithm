e46c0699c72eb6d221e7eccb1f080478e3398ba8
Jaych Su <hi@jaych.su>
2018-01-07 20:14:33 +0800

    2nd, decode_ways

--- lintcode/512_decode_ways.py
+++ lintcode/512_decode_ways.py
@@ -1,10 +1,3 @@
-"""
-Test Case
-
-"0"
-"""
-
-
 class Solution:
     """
     @param: s: a string,  encoded message
@@ -14,24 +7,24 @@ class Solution:
         if not s:
             return 0
 
+        s = s.lower()
         n = len(s)
 
-        # `F[i]` means the decodings way, and it is sum of
-        #   end at `s[i - 1]`
-        # + end at `s[i - 2:i]`
-        F = [0] * (n + 1)
+        """
+        `dp[i]` means the ways to decode
 
-        # ''
-        F[0] = 1
-
-        # the meaning of `0` is same as ''
-        F[1] = 1 if int(s[0]) != 0 else 0
+        `dp[0]` => ''
+        `dp[1]` => should check if the code is 0
+        """
+        dp = [0] * (n + 1)
+        dp[0] = 1
+        dp[1] = 1 if int(s[0]) > 0 else 0
 
         for i in range(2, n + 1):
-            if int(s[i - 1]) != 0:
-                F[i] = F[i - 1]
+            if int(s[i - 1]) > 0:
+                dp[i] = dp[i - 1]
 
             if 10 <= int(s[i - 2:i]) <= 26:
-                F[i] += F[i - 2]
+                dp[i] += dp[i - 2]
 
-        return F[n]
+        return dp[n]
