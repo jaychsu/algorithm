e969173f9f576e8cac209cde02ece78cc8dae9ef
Jaych Su <hi@jaych.su>
2018-01-08 14:37:17 +0800

    2nd, paint_house_ii

--- lintcode/516_paint_house_ii.py
+++ lintcode/516_paint_house_ii.py
@@ -5,7 +5,7 @@ but in doing so, the time complexity is quite high `O(n * k * k)`,
 if the number of colors increases
 
 so in this problem, we need find some way to fix it
-for `dp[curr][j] = dp[prev][k] + costs[i][j]`, `costs[i][j]` is constant
+for `dp[curr][j] = dp[prev][k] + C[i][j]`, `C[i][j]` is constant
 
 so what we want is to find the minimum except the same color
 => if `dp[prev][j]` is the minimum, since we can't use the same color
@@ -14,36 +14,39 @@ so what we want is to find the minimum except the same color
 
 and the time complexity becomes `O(n * 2k)` => `O(nk)`
 """
+
+
 class Solution:
     """
-    @param: costs: n x k cost matrix
+    @param: C: n x k cost matrix
     @return: an integer, the minimum cost to paint all houses
     """
-    def minCostII(self, costs):
-        if not costs:
+    def minCostII(self, C):
+        if not C or not C[0]:
             return 0
 
         INFINITY = float('inf')
-        m, n = len(costs), len(costs[0])
-        dp = [[0] * n for _ in range(2)]
+        n, k = len(C), len(C[0])
+        dp = [[0] * k for _ in range(n)]
+        prev = curr = 0
 
-        i = j = prev = curr = min1 = min2 = 0
+        for j in range(k):
+            dp[curr][j] = C[0][j]
 
-        for j in range(n):
-            dp[0][j] = costs[0][j]
-
-        for i in range(1, m):
+        for i in range(1, n):
             prev = curr
-            curr = i % 2
+            curr = 1 - curr
             min1 = min2 = INFINITY
 
             """
             to find the minimum and second minimum in previous iteration
             """
-            for j in range(n):
+            for j in range(k):
                 if dp[prev][j] < min1:
-                    min2, min1 = min1, dp[prev][j]
-                elif dp[prev][j] < min2:
+                    min2 = min1
+                    min1 = dp[prev][j]
+                    continue
+                if dp[prev][j] < min2:
                     min2 = dp[prev][j]
 
             """
@@ -52,10 +55,11 @@ class Solution:
             and then we need to take the color
             with the second minimum in `dp[prev]`
             """
-            for j in range(n):
-                if min1 == dp[prev][j]:
-                    dp[curr][j] = min2 + costs[i][j]
+            for j in range(k):
+                dp[curr][j] = C[i][j]
+                if dp[prev][j] == min1:
+                    dp[curr][j] += min2
                 else:
-                    dp[curr][j] = min1 + costs[i][j]
+                    dp[curr][j] += min1
 
         return min(dp[curr])
