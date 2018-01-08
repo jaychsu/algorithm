8cec048ac0d540c6a622d2e5fae9ccc1a903bdb7
Jaych Su <hi@jaych.su>
2018-01-08 13:38:15 +0800

    3rd, unique_paths_ii

--- lintcode/115_unique_paths_ii.py
+++ lintcode/115_unique_paths_ii.py
@@ -1,35 +1,32 @@
 class Solution:
     """
-    @param: obstacles: A list of lists of integers
+    @param: G: A list of lists of integers
     @return: An integer
     """
-    def uniquePathsWithObstacles(self, obstacles):
-        m, n = len(obstacles), len(obstacles[0])
+    def uniquePathsWithObstacles(self, G):
+        if not G or not G[0]:
+            return 0
 
+        OBSTACLE = 1
+
+        m, n = len(G), len(G[0])
         dp = [[0] * n for _ in range(2)]
-
         prev = curr = 0
 
         for j in range(n):
-            if obstacles[0][j]:
+            if G[0][j] == OBSTACLE:
                 break
-
-            if j == 0:
-                dp[curr][j] = 1
-                continue
-
-            dp[curr][j] = dp[curr][j - 1]
+            dp[curr][j] = 1
 
         for i in range(1, m):
             prev = curr
             curr = 1 - curr
 
-            dp[curr][0] = dp[prev][0] if not obstacles[i][0] else 0
-
+            dp[curr][0] = 0 if G[i][0] == OBSTACLE else dp[prev][0]
             for j in range(1, n):
-                dp[curr][j] = 0
-
-                if not obstacles[i][j]:
-                    dp[curr][j] = dp[prev][j] + dp[curr][j - 1]
+                if G[i][j] == OBSTACLE:
+                    dp[curr][j] = 0
+                    continue
+                dp[curr][j] = dp[prev][j] + dp[curr][j - 1]
 
         return dp[curr][n - 1]
