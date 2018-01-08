e45a64341c4505c95485e3cbedee1334b574204c
Jaych Su <hi@jaych.su>
2018-01-08 13:09:02 +0800

    2nd, partition_array

--- lintcode/31_partition_array.py
+++ lintcode/31_partition_array.py
@@ -1,21 +1,23 @@
 class Solution:
     """
-    @param: nums: The integer array you should partition
+    @param: A: The integer array you should partition
     @param: k: An integer
     @return: The index after partition
     """
-    def partitionArray(self, nums, k):
-        if not nums:
+    def partitionArray(self, A, k):
+        if not A:
             return 0
 
-        left, right = 0, len(nums) - 1
+        left, right = 0, len(A) - 1
+
         while left <= right:
-            while left <= right and nums[left] < k:
+            while left <= right and A[left] < k:
                 left += 1
-            while left <= right and nums[right] >= k:
+            while left <= right and A[right] >= k:
                 right -= 1
+
             if left <= right:
-                nums[left] = nums[right]
+                A[left], A[right] = A[right], A[left]
                 left += 1
                 right -= 1
 
