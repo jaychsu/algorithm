704a49a187fceb4402936855c2c720fa6f51b2f8
Jaych Su <hi@jaych.su>
2018-01-11 16:53:23 +0800

    2nd, reverse_linked_list_ii

--- lintcode/36_reverse_linked_list_ii.py
+++ lintcode/36_reverse_linked_list_ii.py
@@ -2,7 +2,6 @@
 Definition of ListNode
 
 class ListNode(object):
-
     def __init__(self, val, next=None):
         self.val = val
         self.next = next
@@ -17,47 +16,36 @@ class Solution:
     @return: The head of the reversed ListNode
     """
     def reverseBetween(self, head, m, n):
+        if not head:
+            return
+
         """
-        example: 1->2->3->4->5
-        reverse [3,4]
-        * mp: mth_pre, m: mth, n: nth, nn: nth_nxt
-
-        init/
-        dummy -> 1 -> 2 -> 3 -> 4 -> 5 -> None
-                      mp   m    n    nn
-
-        reverse/
-        dummy -> 1 -> 2  | 4 -> 3 |  5 -> None
-                      mp   n    m    nn
-
-        re-link/
-        dummy -> 1 -> 2 -> 4 -> 3 -> 5 -> None
-                      mp   n    m    nn
+        to get to `n`th node from `m`th node needs `n - m` operations
         """
-        dummy = ListNode(-1, head)
-        mth_pre = self.find_kth(dummy, m - 1)
-        mth = mth_pre.next
-        nth = self.find_kth(dummy, n)
-        nth_nxt = nth.next
-        nth.next = None
+        n -= m
 
-        self.reverse(mth)
-        mth_pre.next = nth
-        mth.next = nth_nxt
-        return dummy.next
+        """
+        `A` will stay at `m-1`th node
+        """
+        A = dummy = ListNode(-1, head)
+        while m > 1:
+            m -= 1
+            A = A.next
 
-    def find_kth(self, head, k):
-        for i in range(k):
-            if not head:
-                return
-            head = head.next
-        return head
-
-    def reverse(self, head):
+        """
+        `B` will stay at `n+1`th node
+        `cur` stay at (`m`th -> `n`th) node
+        """
+        B = cur = A.next
         pre = nxt = None
-        while head:
-            nxt = head.next
-            head.next = pre
-            pre = head
-            head = nxt
-        return pre
+        while n >= 0:
+            n -= 1
+            nxt = B.next
+            B.next = pre
+            pre = B
+            B = nxt
+
+        A.next = pre
+        cur.next = B
+
+        return dummy.next
