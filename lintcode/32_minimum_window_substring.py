38ef8aa7d03ad19d0847d00fd0ff0f213fcd5e6b
Jaych Su <hi@jaych.su>
2018-01-12 17:59:54 +0800

    3rd, minimum_window_substring

--- lintcode/32_minimum_window_substring.py
+++ lintcode/32_minimum_window_substring.py
@@ -1,41 +1,41 @@
 class Solution:
     """
-    @param: source : A string
-    @param: target: A string
+    @param: s: A string
+    @param: t: A string
     @return: A string denote the minimum window, return "" if there is no such a string
     """
-    def minWindow(self, source, target):
-        if not source or not target:
+    def minWindow(self, s, t):
+        if not s or not t:
             return ''
-        n, m = len(source), len(target)
-        l = r = found = 0
-        min_start, min_len = -1, n
-        freq_s, freq_t = {}, dict.fromkeys(target, 0)
-        for c in target: freq_t[c] += 1
-        while r < n:
-            # Find the first matched window in current iteration
-            while r < n and found < m:
-                freq_s[source[r]] = freq_s.get(source[r], 0) + 1
-                if freq_s[source[r]] <= freq_t.get(source[r], 0):
-                    found += 1
-                r += 1
 
-            # Terminate iteration if there is a string in source but not in target
-            if r >= n and found < m:
-                break
+        n = len(s)
+        F = {}
+        for c in t:
+            F[c] = F.get(c, 0) + 1
 
-            # Move forward the left pointer to ignore those mismatched chars
-            while l < r and freq_s[source[l]] > freq_t.get(source[l], 0):
-                freq_s[source[l]] -= 1
-                l += 1
+        min_size = INFINITY = float('inf')
+        min_start = 0
+        cnt = len(F)
+        left = right = 0
 
-            # Record the length of current window if it less than min_len
-            if r - l <= min_len:
-                min_start = l
-                min_len = r - l
+        while right < n:
+            if s[right] in F:
+                F[s[right]] -= 1
+                if F[s[right]] == 0:
+                    cnt -= 1
 
-            # Start to find the next matched window
-            freq_s[source[l]] -= 1
-            l += 1
-            found -= 1
-        return '' if min_start == -1 else source[min_start : min_start + min_len]
+            right += 1
+
+            while cnt == 0:
+                if s[left] in F:
+                    F[s[left]] += 1
+                    if F[s[left]] > 0:
+                        cnt += 1
+
+                if right - left < min_size:
+                    min_size = right - left
+                    min_start = left
+
+                left += 1
+
+        return s[min_start:min_start + min_size] if min_size < INFINITY else ''
