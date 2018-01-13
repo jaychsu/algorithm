7c3cd592c56bc5bd7293beefce7aa2f1964e10c7
Jaych Su <hi@jaych.su>
2018-01-14 03:31:29 +0800

    2nd, 608_two_sum_input_array_is_sorted

--- lintcode/608_two_sum_input_array_is_sorted.py
+++ lintcode/608_two_sum_input_array_is_sorted.py
@@ -4,24 +4,25 @@ space: O(n)
 """
 class Solution:
     """
-    @param: nums: an array of Integer
-    @param: target: target = nums[index1] + nums[index2]
+    @param: A: an array of Integer
+    @param: target: target = A[index1] + A[index2]
     @return: [index1 + 1, index2 + 1] (index1 < index2)
     """
-    def twoSum(self, nums, target):
-        if not nums:
-            return [-1, -1]
+    def twoSum(self, A, target):
+        NOT_FOUND = [-1, -1]
+        if not A:
+            return NOT_FOUND
 
         remaining = {}
-        for i in range(len(nums)):
-            if nums[i] in remaining:
+        for i in range(len(A)):
+            if A[i] in remaining:
                 return [
-                    remaining[nums[i]] + 1,
-                    i + 1
+                    remaining[A[i]] + 1,
+                    i + 1,
                 ]
-            remaining[target - nums[i]] = i
+            remaining[target - A[i]] = i
 
-        return [-1, -1]
+        return NOT_FOUND
 
 
 """
@@ -30,26 +31,23 @@ space: O(1)
 """
 class Solution:
     """
-    @param: nums: an array of Integer
-    @param: target: target = nums[index1] + nums[index2]
+    @param: A: an array of Integer
+    @param: target: target = A[index1] + A[index2]
     @return: [index1 + 1, index2 + 1] (index1 < index2)
     """
-    def twoSum(self, nums, target):
-        if not nums:
-            return [-1, -1]
+    def twoSum(self, A, target):
+        NOT_FOUND = [-1, -1]
+        if not A:
+            return NOT_FOUND
 
-        left, right = 0, len(nums) - 1
-        _sum = 0
+        left, right = 0, len(A) - 1
         while left < right:
-            _sum = nums[left] + nums[right]
+            _sum = A[left] + A[right]
             if _sum == target:
-                return [
-                    left + 1,
-                    right + 1
-                ]
+                return [left + 1, right + 1]
             if _sum < target:
                 left += 1
             else:
                 right -= 1
 
-        return [-1, -1]
+        return NOT_FOUND
