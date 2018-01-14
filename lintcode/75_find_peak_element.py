622ee6e3d2cc5c11e2199b7254738b783c07f216
Jaych Su <hi@jaych.su>
2018-01-14 17:17:39 +0800

    2nd, 75_find_peak_element

--- lintcode/75_find_peak_element.py
+++ lintcode/75_find_peak_element.py
@@ -3,17 +3,16 @@ class Solution:
     @param: A: An integers array.
     @return: return any of peek positions.
     """
-
     def findPeak(self, A):
         if not A:
-            return
-        l, m, r = 1, 1, len(A) - 1
+            return -1
 
-        while l + 1 < r:
-            m = l + (r - l) // 2
-            if A[m] < A[m-1]:
-                r = m
+        left, right = 0, len(A) - 1
+        while left + 1 < right:
+            mid = (left + right) // 2
+            if A[mid] < A[mid + 1]:
+                left = mid
             else:
-                l = m
+                right = mid
 
-        return l if A[l] > A[r] else r
+        return right if A[left] < A[right] else left
