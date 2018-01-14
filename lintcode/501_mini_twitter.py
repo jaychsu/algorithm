9a5b47d30e1a3c2ae6bb514f10beb3b0fbeb3929
Jaych Su <hi@jaych.su>
2018-01-14 10:54:18 +0800

    5th, 501_mini_twitter

--- lintcode/501_mini_twitter.py
+++ lintcode/501_mini_twitter.py
@@ -10,9 +10,9 @@ class Tweet:
 
 class MiniTwitter:
     def __init__(self):
-        self.t = 0
+        self.timestamp = 0
         self.tweets = {}
-        self.friends = {}
+        self.followings = {}
 
     """
     @param: user_id: An integer
@@ -23,11 +23,12 @@ class MiniTwitter:
         if user_id not in self.tweets:
             self.tweets[user_id] = []
 
-        self.t += 1
+        self.timestamp += 1
         self.tweets[user_id].append((
-            self.t,
+            self.timestamp,
             Tweet.create(user_id, tweet_text),
         ))
+
         return self.tweets[user_id][-1][1]
 
     """
@@ -40,16 +41,16 @@ class MiniTwitter:
         if user_id in self.tweets:
             res.extend(self.tweets[user_id][-10:])
 
-        if user_id in self.friends:
-            for friend_id in self.friends[user_id]:
-                if friend_id in self.tweets:
-                    res.extend(self.tweets[friend_id][-10:])
+        if user_id in self.followings:
+            for follow_id in self.followings[user_id]:
+                if follow_id in self.tweets:
+                    res.extend(self.tweets[follow_id][-10:])
 
         if not res:
             return []
 
         res.sort()
-        return [tweet for _, tweet in res][-10:][::-1]
+        return [tweet for _, tweet in res[-10:]][::-1]
 
     """
     @param: user_id: An integer
@@ -59,7 +60,7 @@ class MiniTwitter:
         if user_id not in self.tweets:
             return []
 
-        return [tweet for _, tweet in self.tweets[user_id]][-10:][::-1]
+        return [tweet for _, tweet in self.tweets[user_id][-10:]][::-1]
 
     """
     @param: from_id: An integer
@@ -67,10 +68,13 @@ class MiniTwitter:
     @return: nothing
     """
     def follow(self, from_id, to_id):
-        if from_id not in self.friends:
-            self.friends[from_id] = set()
+        if from_id not in self.followings:
+            self.followings[from_id] = set()
 
-        self.friends[from_id].add(to_id)
+        if to_id in self.followings[from_id]:
+            return
+
+        self.followings[from_id].add(to_id)
 
     """
     @param: from_id: An integer
@@ -78,7 +82,10 @@ class MiniTwitter:
     @return: nothing
     """
     def unfollow(self, from_id, to_id):
-        if from_id not in self.friends:
+        if from_id not in self.followings:
             return
 
-        self.friends[from_id].discard(to_id)
+        if to_id not in self.followings[from_id]:
+            return
+
+        self.followings[from_id].discard(to_id)
