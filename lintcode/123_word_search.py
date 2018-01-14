3e79e577376704a37bcbdb22bcfee37823af6ddb
Jaych Su <hi@jaych.su>
2018-01-14 21:29:16 +0800

    2nd, 123_word_search

--- lintcode/123_word_search.py
+++ lintcode/123_word_search.py
@@ -1,42 +1,45 @@
 class Solution:
-    def __init__(self):
-        self.row_vector = [1, -1, 0, 0]
-        self.col_vector = [0, 0, 1, -1]
+    V = (
+        (-1,  0),
+        ( 1,  0),
+        ( 0, -1),
+        ( 0,  1),
+    )
 
     """
-    @param: board: A list of lists of character
-    @param: word: A string
+    @param: G: A list of lists of character
+    @param: s: A string
     @return: A boolean
     """
-    def exist(self, board, word):
-        if not board or not word or len(board) < 1:
+    def exist(self, G, s):
+        if G is None or G[0] is None or s is None:
             return False
-        self.board, self.word = board, word
-        self.m, self.n, self.l = len(board), len(board[0]), len(word)
-        is_visited = [[0 for j in range(self.n)] for i in range(self.m)]
-        for row in range(self.m):
-            for col in range(self.n):
-                if board[row][col] == word[0] \
-                        and self.find(row, col, 1, is_visited):
-                    # Don't return result of `self.find` directly
-                    # since there might be multiple `word[0]`
+
+        m, n = len(G), len(G[0])
+        visited = [[False] * n for _ in range(m)]
+        for x in range(m):
+            for y in range(n):
+                if (G[x][y] == s[0] and
+                    self.dfs(G, x, y, s, 1, visited)):
                     return True
+
         return False
 
-    def find(self, x, y, i, is_visited):
-        if self.l == i:
+    def dfs(self, G, x, y, s, i, visited):
+        if i >= len(s):
             return True
-        for d in range(4):
-            _x, _y = x + self.row_vector[d], y + self.col_vector[d]
-            if 0 <= _x < self.m \
-                    and 0 <= _y < self.n \
-                    and not is_visited[_x][_y] \
-                    and self.board[_x][_y] == self.word[i]:
-                is_visited[_x][_y] = 1
-                # Don't return result of `self.find` directly
-                # since must reset the cell in `is_visited`
-                if self.find(_x, _y, i + 1, is_visited):
-                    return True
-                else:
-                    is_visited[_x][_y] = 0
+
+        for dx, dy in self.V:
+            _x = x + dx
+            _y = y + dy
+            if not (0 <= _x < len(G) and 0 <= _y < len(G[0])):
+                continue
+            if visited[_x][_y] or G[_x][_y] != s[i]:
+                continue
+
+            visited[_x][_y] = True
+            if self.dfs(G, _x, _y, s, i + 1, visited):
+                return True
+            visited[_x][_y] = False
+
         return False
