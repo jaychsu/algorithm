4f49bf9093bc2da088719276148bf993473ebb32
Jaych Su <hi@jaych.su>
2018-01-15 20:13:27 +0800

    1st, 5_longest_palindromic_substring

---
+++ leetcode/5_longest_palindromic_substring.py
@@ -0,0 +1,28 @@
+class Solution:
+    def longestPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        if not s:
+            return ''
+
+        start = size = 0
+        for i in range(len(s)):
+            _start, _size = self.check_palindrome(s, i, i)
+            __start, __size = self.check_palindrome(s, i, i + 1)
+            if __size > _size:
+                _size = __size
+                _start = __start
+            if _size > size:
+                size = _size
+                start = _start
+
+        return s[start:start + size]
+
+    def check_palindrome(self, s, left, right):
+        n = len(s)
+        while left >= 0 and right < n and s[left] == s[right]:
+            left -= 1
+            right += 1
+        return (left + 1, right - left - 1)
