009bcf3c83bb30bbb3c400606ecd4e47d31abc97
Jaych Su <hi@jaych.su>
2018-01-16 16:38:34 +0800

    1st, 11_container_with_most_water

---
+++ leetcode/11_container_with_most_water.py
@@ -0,0 +1,21 @@
+class Solution:
+    def maxArea(self, H):
+        """
+        :type H: List[int]
+        :rtype: int
+        """
+        ans = 0
+        if not H:
+            return ans
+
+        left, right = 0, len(H) - 1
+        while left < right:
+            area = min(H[left], H[right]) * (right - left)
+            if area > ans:
+                ans = area
+            if H[left] < H[right]:
+                left += 1
+            else:
+                right -= 1
+
+        return ans
