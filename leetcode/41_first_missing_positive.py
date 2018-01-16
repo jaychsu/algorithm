a4a2c367254df43bfe0a493437ab5cb015330ccf
Jaych Su <hi@jaych.su>
2018-01-16 18:08:49 +0800

    1st, 41_first_missing_positive

---
+++ leetcode/41_first_missing_positive.py
@@ -0,0 +1,30 @@
+"""
+REF: http://www.cnblogs.com/yuzhangcmu/p/4200096.html
+"""
+
+
+class Solution:
+    def firstMissingPositive(self, A):
+        """
+        :type A: List[int]
+        :rtype: int
+        """
+        left, right = 0, len(A) - 1
+
+        while left <= right:
+            """
+            for `A[left]`, the index it should be at is `A[left] - 1`
+            1. if it is already at `i` => pass
+            2. if it out of range or duplicated => let `A[right]` in
+            3. if it is legal => swap to let `A[left]` go to `i`
+            """
+            i = A[left] - 1
+            if i == left:
+                left += 1
+            elif i < 0 or i > right or A[i] == A[left]:
+                A[left] = A[right]
+                right -= 1
+            else:
+                A[left], A[i] = A[i], A[left]
+
+        return left + 1
