fcfeb134726d5e7a11f37b8a997e0c10f8ec51ab
Jaych Su <hi@jaych.su>
2018-01-17 16:34:27 +0800

    4th, 386_longest_substring_with_at_most_k_distinct_characters

--- lintcode/386_longest_substring_with_at_most_k_distinct_characters.py
+++ lintcode/386_longest_substring_with_at_most_k_distinct_characters.py
@@ -5,25 +5,29 @@ class Solution:
     @return: An integer
     """
     def lengthOfLongestSubstringKDistinct(self, s, k):
+        ans = 0
         if not s or not k:
-            return 0
+            return ans
 
-        n = len(s)
         F = {}
-        ans = left = right = 0
+        n = len(s)
+        cnt = left = right = 0
 
         while right < n:
-            while right < n and (s[right] in F or len(F) < k):
-                F[s[right]] = F.get(s[right], 0) + 1
-                right += 1
+            F[s[right]] = F.get(s[right], 0) + 1
+            if F[s[right]] == 1:
+                cnt += 1
+
+            right += 1
+
+            while cnt > k:
+                if F[s[left]] == 1:
+                    cnt -= 1
+                F[s[left]] -= 1
+
+                left += 1
 
             if right - left > ans:
                 ans = right - left
-
-            while left < right and len(F) >= k:
-                F[s[left]] -= 1
-                if F[s[left]] == 0:
-                    del F[s[left]]
-                left += 1
 
         return ans
