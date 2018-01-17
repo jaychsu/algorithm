a06e352c10187f2d29bc00eee2b942035da64925
Jaych Su <hi@jaych.su>
2018-01-17 16:36:02 +0800

    3rd, 647_substring_anagrams

--- lintcode/647_substring_anagrams.py
+++ lintcode/647_substring_anagrams.py
@@ -4,36 +4,35 @@ REF: https://leetcode.com/problems/find-all-anagrams-in-a-string/discuss/92007/
 
 
 class Solution:
-    """
-    @param: s: a string
-    @param: t: a string
-    @return: a list of index
-    """
     def findAnagrams(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: List[int]
+        """
         ans = []
         if not s or not t or len(t) > len(s):
             return ans
 
-        D = {}
+        F = {}
         for c in t:
-            D[c] = D.get(c, 0) + 1
+            F[c] = F.get(c, 0) + 1
 
-        n, m = len(s), len(t)
-        cnt = len(D)
+        n, m, cnt = len(s), len(t), len(F)
         left = right = 0
 
         while right < n:
-            if s[right] in D:
-                D[s[right]] -= 1
-                if D[s[right]] == 0:
+            if s[right] in F:
+                F[s[right]] -= 1
+                if F[s[right]] == 0:
                     cnt -= 1
 
             right += 1
 
             while cnt == 0:
-                if s[left] in D:
-                    D[s[left]] += 1
-                    if D[s[left]] > 0:
+                if s[left] in F:
+                    F[s[left]] += 1
+                    if F[s[left]] == 1:
                         cnt += 1
 
                 if right - left == m:
