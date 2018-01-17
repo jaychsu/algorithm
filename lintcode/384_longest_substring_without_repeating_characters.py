1233d20e70b14719af8f17069dad351d90ac2b50
Jaych Su <hi@jaych.su>
2018-01-17 16:32:53 +0800

    5th, 384_longest_substring_without_repeating_characters

--- lintcode/384_longest_substring_without_repeating_characters.py
+++ lintcode/384_longest_substring_without_repeating_characters.py
@@ -1,26 +1,26 @@
 class Solution:
-    """
-    @param: s: a string
-    @return: an integer
-    """
     def lengthOfLongestSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
         ans = 0
         if not s:
             return ans
 
-        n = len(s)
         F = {}
+        n = len(s)
         cnt = left = right = 0
 
         while right < n:
             F[s[right]] = F.get(s[right], 0) + 1
-            if F[s[right]] > 1:
+            if F[s[right]] == 2:
                 cnt += 1
 
             right += 1
 
             while cnt > 0:
-                if F[s[left]] > 1:
+                if F[s[left]] == 2:
                     cnt -= 1
                 F[s[left]] -= 1
 
