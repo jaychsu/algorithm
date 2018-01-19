e671bb6ed4a7c29e978e7a0da4cdb94b0b2c60eb
Jaych Su <hi@jaych.su>
2018-01-19 21:45:06 +0800

    2nd, 119_edit_distance

--- lintcode/119_edit_distance.py
+++ lintcode/119_edit_distance.py
@@ -1,41 +1,36 @@
 class Solution:
-    """
-    @param: A: A string
-    @param: B: A string
-    @return: The minimum number of steps.
-    """
-    def minDistance(self, A, B):
-        if A is None or B is None:
+    def minDistance(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: int
+        """
+        if s is None or t is None:
             return 0
 
-        m, n = len(A), len(B)
+        m, n = len(s), len(t)
 
         """
         `dp[i][j]` means the minimum operations to convert
         the substr end at `A[i - 1]` to
         the substr end at `B[j - 1]`
         """
-        dp = [[0] * (n + 1) for _ in range(2)]
-
-        prev = curr = 0
-
-        for j in range(n + 1):
-            dp[curr][j] = j
+        dp = [[0] * (n + 1) for _ in range(m + 1)]
 
         for i in range(1, m + 1):
-            prev = curr
-            curr = 1 - curr
+            dp[i][0] = i
+        for j in range(1, n + 1):
+            dp[0][j] = j
 
-            dp[curr][0] = i
-
+        for i in range(1, m + 1):
             for j in range(1, n + 1):
                 """
                 no need to init dp[curr][j]
 
                 case 1: no need to do any operations
                 """
-                if A[i - 1] == B[j - 1]:
-                    dp[curr][j] = dp[prev][j - 1]
+                if s[i - 1] == t[j - 1]:
+                    dp[i][j] = dp[i - 1][j - 1]
                     continue
 
                 """
@@ -43,10 +38,10 @@ class Solution:
                 case 3: add `B[j - 1]` to the end of A
                 case 4: replace laster char in A
                 """
-                dp[curr][j] = 1 + min(
-                    dp[prev][j],
-                    dp[curr][j - 1],
-                    dp[prev][j - 1]
+                dp[i][j] = 1 + min(
+                    dp[i - 1][j],
+                    dp[i][j - 1],
+                    dp[i - 1][j - 1]
                 )
 
-        return dp[curr][n]
+        return dp[m][n]
