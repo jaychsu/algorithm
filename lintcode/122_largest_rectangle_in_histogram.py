7ea8ef0428d3dab3b0c47f12fdada283b6e20d84
Jaych Su <hi@jaych.su>
2018-01-19 21:39:03 +0800

    2nd, 122_largest_rectangle_in_histogram

--- lintcode/122_largest_rectangle_in_histogram.py
+++ lintcode/122_largest_rectangle_in_histogram.py
@@ -1,30 +1,91 @@
 """
-Test Case:
-[2,1,5,6,2,3]
-[1,1]
+REF: https://aaronice.gitbooks.io/lintcode/content/data_structure/largest_rectangle_in_histogram.html
+"""
+
+
+"""
+Brute Force: TLE
 """
 class Solution:
-    """
-    @param: height: A list of integer
-    @return: The area of largest rectangle in the histogram
-    """
-    def largestRectangleArea(self, height):
+    def largestRectangleArea(self, H):
+        """
+        :type H: List[int]
+        :rtype: int
+        """
         ans = 0
-        if not height or len(height) < 1:
+        if not H:
             return ans
 
-        # To ensure the last element in monostack will be handled
-        height.append(0)
+        n = len(H)
+        L = [0] * n  # lowest height
 
-        indices = []
-        top = l = h = 0
+        for left in range(n):
+            for right in range(left, n):
+                L[right] = H[right]
 
-        # https://goo.gl/nLfT99
-        for r in range(len(height)):
-            while indices and height[indices[-1]] >= height[r]:
-                top = indices.pop()
-                h = height[top]
-                l = indices[-1] if indices else -1
-                ans = max(ans, h * (r - l - 1))
-            indices.append(r)
+                if right > left and L[right - 1] < H[right]:
+                    L[right] = L[right - 1]
+
+                area = L[right] * (right - left + 1)
+                if area > ans:
+                    ans = area
+
+        return ans
+
+
+"""
+Brute Force with Pruning
+"""
+class Solution:
+    def largestRectangleArea(self, H):
+        """
+        :type H: List[int]
+        :rtype: int
+        """
+        ans = 0
+        if not H:
+            return ans
+
+        n = len(H)
+
+        for right in range(len(H)):
+            if right < n - 1 and H[right] <= H[right + 1]:
+                continue
+            Hmin = H[right]
+            for left in range(right, -1, -1):
+                if H[left] < Hmin:
+                    Hmin = H[left]
+
+                area = Hmin * (right - left + 1)
+                if area > ans:
+                    ans = area
+
+        return ans
+
+
+"""
+Mono-stack
+"""
+class Solution:
+    def largestRectangleArea(self, H):
+        """
+        :type H: List[int]
+        :rtype: int
+        """
+        ans = 0
+        if not H:
+            return ans
+
+        H.append(0)
+        stack = []
+
+        for right in range(len(H)):
+            while stack and H[stack[-1]] >= H[right]:
+                height = H[stack.pop()]
+                left = stack[-1] if stack else -1
+                area = height * (right - left - 1)
+                if area > ans:
+                    ans = area
+            stack.append(right)
+
         return ans
