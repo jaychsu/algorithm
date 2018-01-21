ab86e46e6aee90ba55b461ea16e1a59fc2ca988b
Jaych Su <hi@jaych.su>
2018-01-21 21:11:28 +0800

    3rd, 512_decode_ways

--- lintcode/512_decode_ways.py
+++ lintcode/512_decode_ways.py
@@ -4,10 +4,9 @@ class Solution:
     @return: an integer, the number of ways decoding
     """
     def numDecodings(self, s):
-        if not s:
+        if not s or s == '0':
             return 0
 
-        s = s.lower()
         n = len(s)
 
         """
@@ -18,10 +17,10 @@ class Solution:
         """
         dp = [0] * (n + 1)
         dp[0] = 1
-        dp[1] = 1 if int(s[0]) > 0 else 0
+        dp[1] = 0 if s[0] == '0' else 1
 
         for i in range(2, n + 1):
-            if int(s[i - 1]) > 0:
+            if s[i - 1] != '0':
                 dp[i] = dp[i - 1]
 
             if 10 <= int(s[i - 2:i]) <= 26:
