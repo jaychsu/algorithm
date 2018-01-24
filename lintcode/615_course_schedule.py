dee7e08f51b35353327ff4dd3c189bca4e47024f
Jaych Su <hi@jaych.su>
2018-01-24 16:19:56 +0800

    2nd, 615_course_schedule

--- lintcode/615_course_schedule.py
+++ lintcode/615_course_schedule.py
@@ -1,38 +1,22 @@
 class Solution:
-    """
-    @param: n: a total of n courses
-    @param: P: a list of prerequisite pairs
-    @return: true if can finish all courses or false
-    """
     def canFinish(self, n, P):
-        if not n:
-            return False
-
-        indegs = [0 for _ in range(n)]
+        """
+        :type n: int
+        :type P: List[List[int]]
+        :rtype: bool
+        """
+        indeg = [0] * n
         edges = [[] for _ in range(n)]
-        queue = []
 
-        """
-        c: course, p: prerequisite
-
-        in `edges`, the edge between same both points
-        will only count once.
-        """
         for c, p in P:
+            indeg[c] += 1
             edges[p].append(c)
-            indegs[c] += 1
 
-        for i in range(n):
-            indeg = indegs[i]
-            if indeg == 0:
-                queue.append(i)
-
-        cnt = 0
+        queue = [i for i in range(n) if indeg[i] == 0]
         for p in queue:
-            cnt += 1
             for c in edges[p]:
-                indegs[c] -= 1
-                if indegs[c] == 0:
+                indeg[c] -= 1
+                if indeg[c] == 0:
                     queue.append(c)
 
-        return cnt == n
+        return len(queue) == n
