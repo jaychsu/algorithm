8394f48cfb672a29ac44391a5cc91e0f3d8433b1
Jaych Su <hi@jaych.su>
2018-01-25 18:28:34 +0800

    227_basic_calculator_ii

---
+++ leetcode/227_basic_calculator_ii.py
@@ -0,0 +1,65 @@
+class Solution:
+    def calculate(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        if not s:
+            return 0
+
+        s = self.dal2rpn(s, {
+            '+': 1,
+            '-': 1,
+            '*': 2,
+            '/': 2,
+        })
+
+        if not s:
+            return 0
+
+        return self.eval_rpn(s, {
+            '+': lambda a, b: a + b,
+            '-': lambda a, b: a - b,
+            '*': lambda a, b: a * b,
+            '/': lambda a, b: a / b,
+        })
+
+    def dal2rpn(self, s, P):
+        stack, res = [], []
+
+        for i in range(len(s)):
+            char = s[i]
+            if i > 0 and s[i - 1].isdigit() and char.isdigit():
+                res[-1] += char
+            elif char.isdigit():
+                res.append(char)
+            elif char in P:
+                while (stack and stack[-1] in P and
+                       P[stack[-1]] >= P[char]):
+                    res.append(stack.pop())
+                stack.append(char)
+            elif char == '(':
+                stack.append(char)
+            elif char == ')':
+                while stack and stack[-1] != '(':
+                    res.append(stack.pop())
+                stack.pop()
+
+        while stack:
+            res.append(stack.pop())
+
+        return res
+
+    def eval_rpn(self, s, OP):
+        stack = []
+
+        for char in s:
+            if char.isdigit():
+                stack.append(int(char))
+                continue
+            b = stack.pop()
+            a = stack.pop()
+
+            stack.append(int(OP[char](a, b)))
+
+        return stack[0]
