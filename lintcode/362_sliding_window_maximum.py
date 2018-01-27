82619a308e8a6d64ca7ae693df550e273700808a
Jaych Su <hi@jaych.su>
2018-01-27 18:02:56 +0800

    362_sliding_window_maximum

--- lintcode/362_sliding_window_maximum.py
+++ lintcode/362_sliding_window_maximum.py
@@ -1,74 +1,73 @@
 from heapq import heappush, heappop
 
-class HashHeap:
+
+class HashHeapq:
     def __init__(self):
         self.heap = []
         self.deleted = {}
-        self._len = 0
 
     def push(self, val):
         heappush(self.heap, val)
-        self._len += 1
 
     def pop(self):
-        self._clean_top()
-        self._len -= 1
-        return heappop(self.heap)
+        if self.is_empty():
+            return
+        heappop(self.heap)
 
     def remove(self, val):
+        if self.is_empty():
+            return
         self.deleted[val] = self.deleted.get(val, 0) + 1
-        self._len -= 1
 
     def top(self):
-        self._clean_top()
+        if self.is_empty():
+            return
         return self.heap[0]
 
-    def _clean_top(self):
+    def is_empty(self):
         while self.heap and self.deleted.get(self.heap[0]):
             self.deleted[self.heap[0]] -= 1
             heappop(self.heap)
+        return not self.heap
 
-    def __len__(self):
-        return self._len
-
-    def __repr__(self):
-        return repr(self.heap)
 
 class Solution:
-    """
-    @param: nums: A list of integers
-    @param: k: An integer
-    @return: The maximum number inside the window at each moving
-    """
-    def maxSlidingWindow(self, nums, k):
+    def maxSlidingWindow(self, A, k):
+        """
+        :type A: List[int]
+        :type k: int
+        :rtype: List[int]
+        """
         ans = []
-        if not nums or len(nums) < 1 or not k:
+        if not A:
             return ans
-        self.hashheap = HashHeap()
-        for r in range(len(nums)):
-            self.hashheap.push(- nums[r])
-            if r >= k:
-                self.hashheap.remove(- nums[r - k])
-            if r + 1 >= k:
-                ans.append(- self.hashheap.top())
+
+        heap = HashHeapq()
+
+        for i in range(len(A)):
+            heap.push(-A[i])
+            if i >= k - 1:
+                ans.append(-heap.top())
+                heap.remove(-A[i - k + 1])
+
         return ans
 
 
-'''
+"""
 Failed Solution
 Since its a O(n*k) solution
-'''
+"""
 class Solution:
     """
-    @param: nums: A list of integers
+    @param: A: A list of integers
     @param: k: An integer
     @return: The maximum number inside the window at each moving
     """
-    def maxSlidingWindow(self, nums, k):
+    def maxSlidingWindow(self, A, k):
         ans = []
-        if not nums or len(nums) < 1:
+        if not A or len(A) < 1:
             return ans
-        for r in range(len(nums)):
+        for r in range(len(A)):
             if r >= k - 1:
-                ans.append(max(nums[r - k + 1 : r + 1]))
+                ans.append(max(A[r - k + 1 : r + 1]))
         return ans
