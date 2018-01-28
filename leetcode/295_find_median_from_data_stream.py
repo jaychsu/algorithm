80887a408fe77557a38218ea651ed190a01cb3fb
Jaych Su <hi@jaych.su>
2018-01-29 00:26:54 +0800

    81_data_stream_median

--- leetcode/295_find_median_from_data_stream.py
+++ leetcode/295_find_median_from_data_stream.py
@@ -1,70 +1,41 @@
-'''
-Test Case
+from heapq import heappop, heappush
 
-["MedianFinder","addNum","addNum","findMedian","addNum","findMedian","addNum"]
-[[],[1],[2],[],[3],[],[5]]
-
-["MedianFinder","addNum","findMedian"]
-[[],[1],[]]
-: r > l > 0 -> r > l and r > 0
-: 0 < r < l -> l > r and l > 0
-: r == l -> r == l > 0
-
-["MedianFinder","addNum","findMedian","addNum","findMedian","addNum","findMedian","addNum","findMedian","addNum","findMedian","addNum","findMedian","addNum","findMedian","addNum","findMedian","addNum","findMedian","addNum","findMedian","addNum","findMedian"]
-[[],[6],[],[10],[],[2],[],[6],[],[5],[],[0],[],[6],[],[3],[],[1],[],[0],[],[0],[]]
-: if not num -> if not isinstance(num, int)
-
-'''
-
-from heapq import heappush, heappop
 
 class MedianFinder:
-
     def __init__(self):
-        """
-        initialize your data structure here.
-        """
-        self.max_heap = []
-        self.min_heap = []
+        self.maxheap = []
+        self.minheap = []
 
-    def addNum(self, num):
+    def addNum(self, a):
         """
-        :type num: int
+        :type a: int
         :rtype: void
         """
-        if not isinstance(num, int):
-            return
-        if num > self.findMedian():
-            heappush(self.min_heap, num)
+        if a < self.findMedian():
+            heappush(self.maxheap, -a)
         else:
-            heappush(self.max_heap, - num)
+            heappush(self.minheap, a)
+
+        # to handle odd case, it make sure `minheap` has one more child than `maxheap`
+        while len(self.minheap) > len(self.maxheap) + 1:
+            heappush(self.maxheap, -heappop(self.minheap))
+
+        # to handle even case, it make sure `minheap` and `maxheap` are same size
+        while len(self.maxheap) > len(self.minheap):
+            heappush(self.minheap, -heappop(self.maxheap))
 
     def findMedian(self):
         """
         :rtype: float
         """
-        self.balance()
-        l = len(self.max_heap)
-        r = len(self.min_heap)
-        median = 0
-        if r > l and r > 0:
-            median = self.min_heap[0]
-        elif l > r and l > 0:
-            median = - self.max_heap[0]
-        elif r == l > 0:
-            median = (self.min_heap[0] + self.max_heap[0]) / 2 - self.max_heap[0]
-        return float(median)
+        if not self.minheap:
+            return 0
 
-    def balance(self):
-        l = len(self.max_heap)
-        r = len(self.min_heap)
-        if abs(r - l) <= 1:
-            return
-        if r > l:
-            heappush(self.max_heap, - heappop(self.min_heap))
-        else:
-            heappush(self.min_heap, - heappop(self.max_heap))
-        self.balance()
+        if len(self.minheap) > len(self.maxheap):
+            return self.minheap[0] * 1.0
+
+        # since the child in maxheap is negative
+        return (self.minheap[0] - self.maxheap[0]) / 2.0
 
 
 # Your MedianFinder object will be instantiated and called as such:
--- lintcode/81_data_stream_median.py
+++ lintcode/81_data_stream_median.py
@@ -1,42 +1,39 @@
 from heapq import heappush, heappop
 
+
 class Solution:
     """
-    @param: nums: A list of integers
+    @param: A: A list of integers
     @return: the median of numbers
     """
-    def medianII(self, nums):
+    def medianII(self, A):
         ans = []
-        if not nums or len(nums) < 1:
+        if not A:
             return ans
-        self.min_heap, self.max_heap = [], []
-        for i in range(len(nums)):
-            if ans and nums[i] > ans[-1]:
-                heappush(self.min_heap, nums[i])
+
+        minheap = []
+        maxheap = []
+        median = 0
+
+        for a in A:
+            if a < median:
+                heappush(maxheap, -a)
             else:
-                heappush(self.max_heap, - nums[i])
+                heappush(minheap, a)
 
-            # To compute the median from the dual heap
-            self.balance()
+            while len(minheap) > len(maxheap):
+                heappush(maxheap, -heappop(minheap))
 
-            ans.append(self.get_median())
+            while len(maxheap) > len(minheap) + 1:
+                heappush(minheap, -heappop(maxheap))
+
+            median = -self.find_median(maxheap)
+            ans.append(median)
+
         return ans
 
-    def balance(self):
-        l = len(self.max_heap)
-        r = len(self.min_heap)
-        if abs(r - l) <= 1:
-            return
-        if r > l:
-            heappush(self.max_heap, - heappop(self.min_heap))
-        else:
-            heappush(self.min_heap, - heappop(self.max_heap))
-        self.balance()
+    def find_median(self, maxheap):
+        if not maxheap:
+            return 0
 
-    def get_median(self):
-        l = len(self.max_heap)
-        r = len(self.min_heap)
-        if r > l:
-            return self.min_heap[0]
-        else:
-            return - self.max_heap[0]
+        return maxheap[0]
