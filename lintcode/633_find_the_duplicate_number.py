e2e7833dfbdf379be68dc70f10fc893ed4ff5de7
Jaych Su <hi@jaych.su>
2018-01-29 00:01:20 +0800

    633_find_the_duplicate_number

--- lintcode/633_find_the_duplicate_number.py
+++ lintcode/633_find_the_duplicate_number.py
@@ -1,59 +1,73 @@
+"""
+Binary Searching
+
+`a`: the duplicated num
+`cnt(b)`: the cnt of children less than or equal `b`
+
+for each `num < a`: `cnt(num) == num`
+for each `num >= a`: `cnt(num) > num`
+"""
 class Solution:
-    """
-    @param: A: an array containing n + 1 integers which is between 1 and n
-    @return: the duplicate one
-    """
     def findDuplicate(self, A):
         """
-        `a`: the duplicated num
-        `cnt(b)`: the cnt of children less than or equal `b`
-
-        for each `num < a`: `cnt(num) == num`
-        for each `num >= a`: `cnt(num) > num`
+        :type A: List[int]
+        :rtype: int
         """
-        left, right = 1, len(A) - 1  # since `len(A) == n + 1`
+        if not A:
+            return -1
+
+        left, right = 1, len(A) - 1
+
         while left + 1 < right:
             mid = (left + right) // 2
-            if self.check_valid(mid, A):
+            if self.after_dup(A, mid):
                 right = mid
             else:
                 left = mid
 
-        return left if self.check_valid(left, A) else right
+        if self.after_dup(A, left):
+            return left
+        if self.after_dup(A, right):
+            return right
+        return -1
 
-    def check_valid(self, mid, A):
+    def after_dup(self, A, mid):
         cnt = 0
+
         for a in A:
             if a <= mid:
                 cnt += 1
             if cnt > mid:
                 return True
+
         return False
 
 
+"""
+Floyd Circle Detection
+
+example: [5,4,4,3,2,1]
+
+i  0, 1, 2, 3, 4, 5
+a  5, 4, 4, 3, 2, 1
+-------------------
+   s              f
+      f           s
+      s  f
+         f     s
+         sf
+-------------------
+   f     s
+               s  f
+      f  s
+               sf
+"""
 class Solution:
     """
     @param: A: an array containing n + 1 integers which is between 1 and n
     @return: the duplicate one
     """
     def findDuplicate(self, A):
-        """
-        example: [5,4,4,3,2,1]
-
-        i  0, 1, 2, 3, 4, 5
-        a  5, 4, 4, 3, 2, 1
-        -------------------
-           s              f
-              f           s
-              s  f
-                 f     s
-                 sf
-        -------------------
-           f     s
-                       s  f
-              f  s
-                       sf
-        """
         if not A:
             return -1
 
