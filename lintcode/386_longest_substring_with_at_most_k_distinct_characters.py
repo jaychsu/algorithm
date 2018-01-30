e10790bedb38ced7214a0ed29a97127eaa4be041
Jaych Su <hi@jaych.su>
2018-01-30 19:12:31 +0800

    386_longest_substring_with_at_most_k_distinct_characters

--- lintcode/386_longest_substring_with_at_most_k_distinct_characters.py
+++ lintcode/386_longest_substring_with_at_most_k_distinct_characters.py
@@ -6,11 +6,12 @@ class Solution:
     """
     def lengthOfLongestSubstringKDistinct(self, s, k):
         ans = 0
-        if not s or not k:
+        if not s:
             return ans
 
         F = {}
         n = len(s)
+
         cnt = left = right = 0
 
         while right < n:
@@ -21,9 +22,9 @@ class Solution:
             right += 1
 
             while cnt > k:
-                if F[s[left]] == 1:
-                    cnt -= 1
                 F[s[left]] -= 1
+                if F[s[left]] == 0:
+                    cnt -= 1
 
                 left += 1
 
