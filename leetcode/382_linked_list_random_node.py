61c0c04f5ad923a052337a7bb510f98f1264f3ea
Jaych Su <hi@jaych.su>
2018-01-30 19:46:49 +0800

    382_linked_list_random_node

---
+++ leetcode/382_linked_list_random_node.py
@@ -0,0 +1,39 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+from random import randint
+
+
+class Solution:
+    def __init__(self, head):
+        """
+        @param head The linked list's head.
+        Note that the head is guaranteed to be not null, so it contains at least one node.
+        :type head: ListNode
+        """
+        self.head = head
+
+    def getRandom(self):
+        """
+        Returns a random node's value.
+        :rtype: int
+        """
+        res = self.head
+        node = res.next
+        i = 1
+
+        while node:
+            if randint(0, i) == 0:
+                res = node
+            node = node.next
+            i += 1
+
+        return res.val
+
+
+# Your Solution object will be instantiated and called as such:
+# obj = Solution(head)
+# param_1 = obj.getRandom()
