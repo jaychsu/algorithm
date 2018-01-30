cd4fdce0f17a1b2d964ac8dfa41142485ceb53e0
Jaych Su <hi@jaych.su>
2018-01-30 19:38:45 +0800

    548_intersection_of_two_arrays_ii

--- lintcode/548_intersection_of_two_arrays_ii.py
+++ lintcode/548_intersection_of_two_arrays_ii.py
@@ -1,4 +1,28 @@
 class Solution:
+    def intersect(self, A, B):
+        """
+        :type A: List[int]
+        :type B: List[int]
+        :rtype: List[int]
+        """
+        ans = []
+        if not A or not B:
+            return ans
+
+        F = {}
+        for a in B:
+            F[a] = F.get(a, 0) + 1
+
+        for a in A:
+            if not F.get(a):
+                continue
+            ans.append(a)
+            F[a] -= 1
+
+        return ans
+
+
+class Solution:
     """
     @param: A: an integer array
     @param: B: an integer array
