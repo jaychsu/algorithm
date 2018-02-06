36efaa95a7032bcb55ecf0cc4ee02de15dca02ae
Jaych Su <hi@jaych.su>
2018-02-06 08:09:57 +0800

    20_valid_parentheses.py

--- leetcode/20_valid_parentheses.py
+++ leetcode/20_valid_parentheses.py
@@ -9,16 +9,16 @@ class Solution:
         if not s:
             return False
 
-        B = {')': '(', ']': '[', '}': '{'}
-
+        P = {')': '(', ']': '[', '}': '{'}
         stack = []
+
         for c in s:
             if c in ('(', '[', '{'):
                 stack.append(c)
                 continue
             if c not in (')', ']', '}'):
                 return False
-            if not stack or stack.pop() != B[c]:
+            if not stack or P[c] != stack.pop():
                 return False
 
         return len(stack) == 0
