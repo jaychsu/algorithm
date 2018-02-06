68c18e6e834b1b3795951830173630a345050abd
Jaych Su <hi@jaych.su>
2018-02-06 08:03:44 +0800

    14_longest_common_prefix.py

--- leetcode/14_longest_common_prefix.py
+++ leetcode/14_longest_common_prefix.py
@@ -7,18 +7,9 @@ class Solution:
         if not S:
             return ''
 
-        """
-        i: `i`th char in `S[0]`
-        j: `j`th word in `S`
-        """
         for i in range(len(S[0])):
-            char = S[0][i]
-            for j in range(1, len(S)):
-                if i >= len(S[j]) or S[j][i] != char:
+            for word in S:
+                if i >= len(word) or word[i] != S[0][i]:
                     return S[0][:i]
 
-        """
-        if process reach here
-        means `len(S) == 1 and len(S[0]) == 1`
-        """
         return S[0]
