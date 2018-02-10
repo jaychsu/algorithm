cdf34a618971c9d809f06b1505dcbb4ec8d59204
Jaych Su <hi@jaych.su>
2018-02-10 19:48:32 +0800

    656_big_integer_multiplication.py

--- lintcode/656_big_integer_multiplication.py
+++ lintcode/656_big_integer_multiplication.py
@@ -1,26 +1,26 @@
 class Solution:
-    """
-    @param: A: a non-negative integers
-    @param: B: a non-negative integers
-    @return: return product of A and B
-    """
-    def multiply(self, A, B):
-        if not A or not B or A == '0' or B == '0':
+    def multiply(self, a, b):
+        """
+        :type a: str
+        :type b: str
+        :rtype: str
+        """
+        if not a or not b or a == '0' or b == '0':
             return '0'
 
-        m, n = len(A), len(B)
+        m, n = len(a), len(b)
         tmp = [0] * (m + n)
 
         for i in range(m - 1, -1, -1):
             carry = 0
             for j in range(n - 1, -1, -1):
-                product = carry + tmp[i + j + 1] + int(A[i]) * int(B[j])
-                tmp[i + j + 1] = product % 10
-                carry = product // 10
+                carry += tmp[i + j + 1] + int(a[i]) * int(b[j])
+                tmp[i + j + 1] = carry % 10
+                carry //= 10
             tmp[i] = carry
 
         i = 0
         while tmp[i] == 0:
             i += 1
 
-        return ''.join([str(a) for a in tmp[i:]])
+        return ''.join([str(j) for j in tmp[i:]])
