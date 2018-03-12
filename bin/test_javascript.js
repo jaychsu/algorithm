4c288d26c47dc6d1e8318aa7590c63d8c5cd748b
Jaych Su <hi@jaych.su>
2018-03-12 16:06:13 +0800

    Config test env for javascript

---
+++ bin/test_javascript.js
@@ -0,0 +1,6 @@
+// TODO: wrap this file to a executable command
+// [Configuring Jest](https://facebook.github.io/jest/docs/en/configuration.html)
+module.exports = {
+  "rootDir": "..",
+  "verbose": true
+}
