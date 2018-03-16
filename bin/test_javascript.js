46aa5c2e07fd652430601f978a7e6deb12aae06e
Jaych Su <hi@jaych.su>
2018-03-16 14:07:04 +0800

    Limit the scope to run javascript test

--- bin/test_javascript.js
+++ bin/test_javascript.js
@@ -2,5 +2,6 @@
 // [Configuring Jest](https://facebook.github.io/jest/docs/en/configuration.html)
 module.exports = {
   "rootDir": "..",
+  "testRegex": "/(other|topic)/.*(\\.|/)(test|spec)\\.jsx?$",
   "verbose": true
 }
