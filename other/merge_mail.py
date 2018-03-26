d835854ded26cb41a4c671c12c97de598bcdbc32
Jaych Su <hi@jaych.su>
2018-03-26 20:21:25 +0800

    Refactoring `other/*.py`

--- other/merge_mail.py
+++ other/merge_mail.py
@@ -1,58 +1,55 @@
-class Solution:
-    def merge_mail(self, data):
-        """group owners if their mails got a connection
-        >>> s = Solution()
+def merge_mail(data):
+    """group owners if their mails got a connection
+    >>> merge_mail({
+    ...     'A1': ['a1@gmail.com', 'a2@gmail.com'],
+    ...     'A2': ['b1@gmail.com', 'a2@gmail.com'],
+    ...     'A3': ['c1@gmail.com'],
+    ...     'A4': ['c1@gmail.com', 'd1@gmail.com'],
+    ...     'A5': ['b1@gmail.com', 'e1@gmail.com'],
+    ... })
+    [['A1', 'A2', 'A5'], ['A3', 'A4']]
 
-        >>> s.merge_mail({
-        ...     'A1': ['a1@gmail.com', 'a2@gmail.com'],
-        ...     'A2': ['b1@gmail.com', 'a2@gmail.com'],
-        ...     'A3': ['c1@gmail.com'],
-        ...     'A4': ['c1@gmail.com', 'd1@gmail.com'],
-        ...     'A5': ['b1@gmail.com', 'e1@gmail.com'],
-        ... })
-        [['A1', 'A2', 'A5'], ['A3', 'A4']]
+    >>> merge_mail({
+    ...     'A1': ['a1@gmail.com', 'a2@gmail.com'],
+    ...     'A2': ['b1@gmail.com', 'a2@gmail.com'],
+    ...     'A3': ['b1@gmail.com', 'c1@gmail.com'],
+    ...     'A4': ['c1@gmail.com', 'd1@gmail.com'],
+    ...     'A5': ['b1@gmail.com', 'e1@gmail.com'],
+    ... })
+    [['A1', 'A2', 'A3', 'A4', 'A5']]
+    """
+    owners = {}  # owners
+    mail2owners = {}  # mail to owners
 
-        >>> s.merge_mail({
-        ...     'A1': ['a1@gmail.com', 'a2@gmail.com'],
-        ...     'A2': ['b1@gmail.com', 'a2@gmail.com'],
-        ...     'A3': ['b1@gmail.com', 'c1@gmail.com'],
-        ...     'A4': ['c1@gmail.com', 'd1@gmail.com'],
-        ...     'A5': ['b1@gmail.com', 'e1@gmail.com'],
-        ... })
-        [['A1', 'A2', 'A3', 'A4', 'A5']]
-        """
-        owners = {}  # owners
-        mail2owners = {}  # mail to owners
+    for owner, mails in data.items():
+        find(owners, owner)
+        for mail in mails:
+            if mail not in mail2owners:
+                mail2owners[mail] = owner
+                continue
+            mail2owners[mail] = connect(
+                owners, owner, mail2owners[mail]
+            )
 
-        for owner, mails in data.items():
-            self.find(owners, owner)
-            for mail in mails:
-                if mail not in mail2owners:
-                    mail2owners[mail] = owner
-                    continue
-                mail2owners[mail] = self.connect(
-                    owners, owner, mail2owners[mail]
-                )
+    ans = {}
+    for k, v in owners.items():
+        if v not in ans:
+            ans[v] = []
+        ans[v].append(k)
+    return list(ans.values())
 
-        ans = {}
-        for k, v in owners.items():
-            if v not in ans:
-                ans[v] = []
-            ans[v].append(k)
-        return list(ans.values())
+def connect(nodes, a, b):
+    _a = find(nodes, a)
+    _b = find(nodes, b)
+    if _a is not _b:
+        nodes[_a] = _b
+    return _b
 
-    def connect(self, nodes, a, b):
-        _a = self.find(nodes, a)
-        _b = self.find(nodes, b)
-        if _a is not _b:
-            nodes[_a] = _b
-        return _b
-
-    def find(self, nodes, a):
-        if a not in nodes:
-            nodes[a] = a
-            return a
-        if nodes[a] is a:
-            return a
-        nodes[a] = self.find(nodes, nodes[a])
-        return nodes[a]
+def find(nodes, a):
+    if a not in nodes:
+        nodes[a] = a
+        return a
+    if nodes[a] is a:
+        return a
+    nodes[a] = find(nodes, nodes[a])
+    return nodes[a]
--- other/uneven_random_get.py
+++ other/uneven_random_get.py
@@ -2,38 +2,36 @@
 import random
 
 
-class Solution:
-    def uneven_random_get(self, options, rate):
-        """unevenly fetch the option according to the corresponding rate
-        :type options: list[str]
-        :type rate: list[num]
-        :rtype: str
+def uneven_random_get(options, rate):
+    """unevenly fetch the option according to the corresponding rate
+    :type options: list[str]
+    :type rate: list[num]
+    :rtype: str
 
-        >>> s = Solution()
-        >>> options = ['a', 'b', 'c']
+    >>> options = ['a', 'b', 'c']
 
-        >>> ans = dict.fromkeys(options, 0)
-        >>> for _ in range(10000):
-        ...     c = s.uneven_random_get(options, [33, 33, 34])
-        ...     ans[c] += 1
-        >>> [2000 <= ans[c] <= 4000 for c in options]
-        [True, True, True]
+    >>> ans = dict.fromkeys(options, 0)
+    >>> for _ in range(10000):
+    ...     c = uneven_random_get(options, [10, 10, 10])
+    ...     ans[c] += 1
+    >>> [2000 <= ans[c] <= 4000 for c in options]
+    [True, True, True]
 
-        >>> ans = dict.fromkeys(options, 0)
-        >>> for _ in range(10000):
-        ...     c = s.uneven_random_get(options, [90, 10, 10])
-        ...     ans[c] += 1
-        >>> [8000 <= ans['a'] <= 10000, 0 <= ans['b'] <= 2000, 0 <= ans['c'] <= 2000]
-        [True, True, True]
-        """
-        if not options or not rate or len(options) != len(rate):
-            return ''
+    >>> ans = dict.fromkeys(options, 0)
+    >>> for _ in range(10000):
+    ...     c = uneven_random_get(options, [80, 10, 10])
+    ...     ans[c] += 1
+    >>> [7000 <= ans['a'] <= 9000, 0 <= ans['b'] <= 2000, 0 <= ans['c'] <= 2000]
+    [True, True, True]
+    """
+    if not options or not rate or len(options) != len(rate):
+        return ''
 
-        num = 0
-        rand = random.randint(1, sum(rate))
+    num = 0
+    rand = random.randint(1, sum(rate))
 
-        for i in range(len(rate)):
-            num += rate[i]
+    for i in range(len(rate)):
+        num += rate[i]
 
-            if rand <= num:
-                return options[i]
+        if rand <= num:
+            return options[i]
