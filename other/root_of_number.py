69e0b9b03bbde9cfd7be943ed56b5bbace8209f3
Jaych Su <hi@jaych.su>
2018-04-10 17:54:19 +0800

    Solve problem

---
+++ other/root_of_number.py
@@ -0,0 +1,46 @@
+"""
+>>> gotcha = []
+>>> for _in, _out in (
+...     ((4, 2), 2),
+...     ((9, 3), 2.08),
+...     ((7, 3), 1.913),
+...     ((11, 4), 1.821),
+... ):
+...     res = root(*_in)
+...     valid = abs(res - _out) < 0.001
+...     if not valid: print(_in, res)
+...     gotcha.append(valid)
+>>> bool(gotcha) and all(gotcha)
+True
+"""
+
+
+def root(x, n):
+    if not x or x in (0, 1):
+        return x
+
+    left = 0
+    right = x if x > 1 else 1
+
+    while right - left > 1e-4:
+        mid = (left + right) / 2
+        product = get_product(mid, n)
+
+        if product < x:
+            left = mid
+        elif product > x:
+            right = mid
+        else:
+            return mid
+
+    return left
+
+
+def get_product(x, n):
+    res = 1
+
+    while n:
+        res *= x
+        n -= 1
+
+    return res
