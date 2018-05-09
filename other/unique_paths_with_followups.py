c6f6c8e81aad557b8bc520b1a9298d201462c20f
Jaych Su <hi.jaychsu@gmail.com>
2018-05-09 09:47:06 +0800

    Refactoring `other/unique_paths_with_followups.py`

--- other/unique_paths_with_followups.py
+++ other/unique_paths_with_followups.py
@@ -1,82 +1,42 @@
 """
-Description:
-给一个grid的宽和长，求所有可能的路径数量。
+给定一个矩形的宽和长，求所有可能的路径数量
 
-1. unique_paths_from_three_dir: 从左下的点到右下的点，要求每次只能走三个方向 →↗↘
+Rules：
+1. 从左上角走到右上角
+2. 要求每一步只能向正右、右上或右下走，即 →↗↘
 
-unique_paths_from_three_dir_1
-follow up 1: 2d dp -> 1d dp (
-    dp = [0] * m
-    since the iterate order, for dp[x + 1] may modified by previous iteration
-    so we need maintain two pointer `pre`, `cur` to save origin val in `dp[x]`
-    and use this val in next iteration, that is `dp[x + 1]` for next
-)
-
-
-2. unique_paths_from_three_dir2: 从左上角走到右上角 (每一步，只能向正右、右上 或 右下走 →↗↘)
-
-follow up 1：如果给矩形里的三个点，要求解决上述问题的同时，遍历这三个点 (
-    unique_paths_from_three_dir2_1_1
-    1. 切割矩形，一个一个地做DP，然后相加 (需要注意再进行下一个矩形遍历前，要把同 col 的其他值归零)
-
-    unique_paths_from_three_dir2_1_2
- or 2. 在遍历到指定点时，把同一 col 的其他点归零 (因为 wrap 是沿 y 遍历，所以取 same col)
-)
-
-unique_paths_from_three_dir2_2
-follow up 2：如何判断这三个点一个是合理的，即存在遍历这三个点的路经 (
-    观察得知，把点集合按 y 升序排序，并假设某点座标在 x, y
-    则此点的前一点必须位于 (呈现一个三角形)
-        if y-1: [x-1, x+1]
-        if y-2: [x-2, x+2]
-        if y'=y-n: [x-n, x+n]
-        ... => get dy = y-y' = n, x' MUST in [x-n, x+n]
-    从 y 最大的点往前遍历检查前一点
-)
-
-unique_paths_from_three_dir2_3
-follow up 3：如果给你一个 h，要求你的路径必须向下越过 h 这个界(x >= h)，怎么做 (
-    1. 先遍历一遍超过 h 的，并直接到最底端
-    2. 再把低于 h 的归零
-    3. 再从 x = h - 1 遍历到 target，
-       因为第一次的遍历已经把能到达 x >= h (即 [h, m - 1]) 的方法数找到了
-       从 m - 1 开始的话，会有重复计算
-)
-
-
-Main Concept:
-1. similiar to unique_path, but need to change iterative order
-
-2. transition equation:
-→: dp[x][y - 1]
-↗: dp[x + 1][y - 1]
-↘: dp[x - 1][y - 1]
+followup1: 优化空间复杂度至 O(n)
+followup2: 给定矩形里的三个点，判断是否存在遍历这三个点的路经
+followup3: 给定矩形里的三个点，找到遍历这三个点的所有路径数量
+followup4: 给定一个下界 (x == H)，找到能经过给定下界的所有路径数量 (x >= H)
+followup5: 起点和终点改成从左上到左下，每一步只能 ↓↘↙，求所有可能的路径数量
 """
 
 
-def unique_paths_from_three_dir(m, n):
+def find_unique_paths(m, n):
     """
     :type m: int
     :type n: int
     :rtype: int
 
-    >>> unique_paths_from_three_dir(2, 2)
-    1
-    >>> unique_paths_from_three_dir(2, 3)
-    2
-    >>> unique_paths_from_three_dir(3, 3)
-    2
-    >>> unique_paths_from_three_dir(4, 4)
-    4
+    >>> gotcha = [
+    ...     find_unique_paths(*_in) == _out
+    ...     for _in, _out in (
+    ...         ((2, 2), 1), ((2, 3), 2), ((3, 3), 2),
+    ...         ((5, 5), 9), ((7, 6), 21), ((6, 7), 51),
+    ...     )
+    ... ]
+    >>> bool(gotcha) and all(gotcha)
+    True
     """
     if not m or not n:
         return 0
 
     dp = [[0] * n for _ in range(m)]
-    dp[m - 1][0] = 1
+    dp[0][0] = 1
 
     for y in range(1, n):
-        for x in range(m - 1, -1, -1):
+        for x in range(m):
             dp[x][y] = dp[x][y - 1]
 
             if x > 0:
@@ -85,233 +45,166 @@ def unique_paths_from_three_dir(m, n):
             if x + 1 < m:
                 dp[x][y] += dp[x + 1][y - 1]
 
-    return dp[m - 1][n - 1]
+    return dp[0][n - 1]
 
 
-def unique_paths_from_three_dir_1(m, n):
+def find_unique_paths1(m, n):
     """
     :type m: int
     :type n: int
     :rtype: int
 
-    >>> unique_paths_from_three_dir_1(2, 2)
-    1
-    >>> unique_paths_from_three_dir_1(2, 3)
-    2
-    >>> unique_paths_from_three_dir_1(3, 3)
-    2
-    >>> unique_paths_from_three_dir_1(4, 4)
-    4
+    >>> gotcha = [
+    ...     find_unique_paths1(*_in) == _out
+    ...     for _in, _out in (
+    ...         ((2, 2), 1), ((2, 3), 2), ((3, 3), 2),
+    ...         ((5, 5), 9), ((7, 6), 21), ((6, 7), 51),
+    ...     )
+    ... ]
+    >>> bool(gotcha) and all(gotcha)
+    True
     """
     if not m or not n:
         return 0
 
     dp = [0] * m
-    dp[m - 1] = 1
+    dp[0] = 1
+    pre = cur = 0
 
     for y in range(1, n):
         pre = cur = 0
 
-        for x in range(m - 1, -1, -1):
+        for x in range(m):
             pre, cur = cur, dp[x]
 
             if x > 0:
-                dp[x] += dp[x - 1]
-
-            if x + 1 < m:
                 dp[x] += pre
 
-    return dp[m - 1]
-
-
-def unique_paths_from_three_dir2(m, n):
-    """
-    :type m: int
-    :type n: int
-    :rtype: int
-
-    >>> unique_paths_from_three_dir2(2, 2)
-    1
-    >>> unique_paths_from_three_dir2(2, 3)
-    2
-    >>> unique_paths_from_three_dir2(3, 3)
-    2
-    >>> unique_paths_from_three_dir2(4, 4)
-    4
-    """
-    if not m or not n:
-        return 0
-
-    dp = [[0] * n for _ in range(m)]
-    dp[0][0] = 1
-
-    for y in range(1, n):
-        for x in range(m):
-            dp[x][y] = dp[x][y - 1]
-
-            if x > 0:
-                dp[x][y] += dp[x - 1][y - 1]
-
             if x + 1 < m:
-                dp[x][y] += dp[x + 1][y - 1]
+                dp[x] += dp[x + 1]
 
-    return dp[0][n - 1]
-
-
-def unique_paths_from_three_dir2_1_1(m, n, points):
-    """
-    :type m: int
-    :type n: int
-    :type points: list[list[int]]
-    :rtype: int
-
-    >>> unique_paths_from_three_dir2_1_1(2, 3, [[1, 0], [1, 1], [1, 2]])
-    0
-    >>> unique_paths_from_three_dir2_1_1(3, 3, [[1, 0], [2, 1], [1, 2]])
-    0
-    >>> unique_paths_from_three_dir2_1_1(5, 5, [[0, 1], [2, 2], [1, 3]])
-    0
-    >>> unique_paths_from_three_dir2_1_1(5, 5, [[1, 1], [2, 2], [1, 3]])
-    1
-    >>> unique_paths_from_three_dir2_1_1(5, 5, [[2, 2], [1, 1], [1, 3]])
-    1
-    """
-    if not m or not n or not points:
-        return 0
-
-    points = sorted(
-        [p for p in points if p[1] != 0],
-        key=lambda p: (p[1], p[0])
-    )
-
-    if len(points) != 3:
-        return 0
-
-    dp = [[0] * n for _ in range(m)]
-    dp[0][0] = 1
-    pi = 0
-
-    for y in range(1, n):
-        for x in range(m):
-            dp[x][y] = dp[x][y - 1]
-
-            if x > 0:
-                dp[x][y] += dp[x - 1][y - 1]
-
-            if x + 1 < m:
-                dp[x][y] += dp[x + 1][y - 1]
-
-        if pi < 3 and y == points[pi][1]:
-            for x in range(m):
-                if x != points[pi][0]:
-                    dp[x][y] = 0
-            pi += 1
-
-    return dp[0][n - 1]
+    return dp[0]
 
 
-def unique_paths_from_three_dir2_1_2(m, n, points):
-    """
-    :type m: int
-    :type n: int
-    :type points: list[list[int]]
-    :rtype: int
-
-    >>> unique_paths_from_three_dir2_1_2(2, 3, [[1, 0], [1, 1], [1, 2]])
-    0
-    >>> unique_paths_from_three_dir2_1_2(3, 3, [[1, 0], [2, 1], [1, 2]])
-    0
-    >>> unique_paths_from_three_dir2_1_2(5, 5, [[0, 1], [2, 2], [1, 3]])
-    0
-    >>> unique_paths_from_three_dir2_1_2(5, 5, [[1, 1], [2, 2], [1, 3]])
-    1
-    >>> unique_paths_from_three_dir2_1_2(5, 5, [[2, 2], [1, 1], [1, 3]])
-    1
-    """
-    if not m or not n or not points:
-        return 0
-
-    def modify_dp(dp, x1, y1, x2, y2):
-        RANGE = range(x1, x2 + 1) if x2 >= x1 else range(x1, x2 - 1, -1)
-
-        for x in range(len(dp)):
-            if x != x1:
-                dp[x][y1] = 0
-
-        for y in range(y1 + 1, y2 + 1):
-            for x in RANGE:
-                dp[x][y] = dp[x][y - 1]
-
-                if x > 0:
-                    dp[x][y] += dp[x - 1][y - 1]
-
-                if x + 1 < m:
-                    dp[x][y] += dp[x + 1][y - 1]
-
-    points.sort(key=lambda p: (p[1], p[0]))
-
-    dp = [[0] * n for _ in range(m)]
-    dp[0][0] = 1
-    modify_dp(dp, 0, 0, points[0][0], points[0][1])
-    modify_dp(dp, points[0][0], points[0][1], points[1][0], points[1][1])
-    modify_dp(dp, points[1][0], points[1][1], points[2][0], points[2][1])
-    modify_dp(dp, points[2][0], points[2][1], 0, n - 1)
-    return dp[0][n - 1]
-
-
-def unique_paths_from_three_dir2_2(m, n, points):
+def find_unique_paths2(m, n, points):
     """
     :type m: int
     :type n: int
     :type points: list[list[int]]
     :rtype: bool
 
-    >>> unique_paths_from_three_dir2_2(2, 3, [[1, 0], [1, 1], [1, 2]])
-    False
-    >>> unique_paths_from_three_dir2_2(3, 3, [[1, 0], [2, 1], [1, 2]])
-    False
-    >>> unique_paths_from_three_dir2_2(5, 5, [[0, 1], [2, 2], [1, 3]])
-    False
-    >>> unique_paths_from_three_dir2_2(5, 5, [[1, 1], [2, 2], [1, 3]])
-    True
-    >>> unique_paths_from_three_dir2_2(5, 5, [[2, 2], [1, 1], [1, 3]])
+    >>> gotcha = [
+    ...     find_unique_paths2(*_in) == _out
+    ...     for _in, _out in (
+    ...         ((2, 3, [[1, 0], [1, 1], [1, 2]]), False),
+    ...         ((3, 3, [[1, 0], [2, 1], [1, 2]]), False),
+    ...         ((5, 5, [[0, 1], [2, 2], [1, 3]]), False),
+    ...         ((5, 5, [[1, 1], [2, 2], [1, 3]]), True),
+    ...         ((5, 5, [[2, 2], [1, 1], [1, 3]]), True),
+    ...         ((6, 8, [[0, 0], [4, 3], [0, 7]]), False),
+    ...         ((8, 6, [[1, 1], [0, 4], [1, 3]]), True),
+    ...         ((8, 6, [[1, 1], [0, 4], [2, 3]]), False),
+    ...     )
+    ... ]
+    >>> bool(gotcha) and all(gotcha)
     True
     """
-    paths = [(0, 0)]
-    points.sort(key=lambda p: (p[1], p[0]))
-    paths.extend(tuple(p) for p in points)
-    paths.append((0, n - 1))
+    if not m or not n or not points or len(points) < 3:
+        return False
 
-    for i in range(1, len(paths)):
-        x, y = paths[i]
-        _x, _y = paths[i - 1]
+    path = [(0, 0), (0, n - 1)]
+    path.extend(tuple(p) for p in points)
+    path.sort(key=lambda p: (p[1], p[0]))
+
+    for i in range(1, len(path)):
+        x, y = path[i]
+        _x, _y = path[i - 1]
         delta = y - _y
 
-        if not x - delta <= _x <= x + delta:
+        if not (x - delta <= _x <= x + delta):
             return False
 
     return True
 
 
-def unique_paths_from_three_dir2_3(m, n, h):
+def find_unique_paths3(m, n, points):
+    """
+    :type m: int
+    :type n: int
+    :type points: list[list[int]]
+    :rtype: int
+
+    >>> gotcha = [
+    ...     find_unique_paths3(*_in) == _out
+    ...     for _in, _out in (
+    ...         ((2, 3, [[1, 0], [1, 1], [1, 2]]), 0),
+    ...         ((3, 3, [[1, 0], [2, 1], [1, 2]]), 0),
+    ...         ((5, 5, [[0, 1], [2, 2], [1, 3]]), 0),
+    ...         ((5, 5, [[1, 1], [2, 2], [1, 3]]), 1),
+    ...         ((5, 5, [[2, 2], [1, 1], [1, 3]]), 1),
+    ...         ((6, 8, [[0, 0], [4, 3], [0, 7]]), 0),
+    ...         ((8, 6, [[0, 0], [0, 5], [0, 4]]), 9),
+    ...         ((8, 6, [[1, 1], [0, 4], [2, 3]]), 0),
+    ...     )
+    ... ]
+    >>> bool(gotcha) and all(gotcha)
+    True
+    """
+    NOT_FOUND = 0
+
+    if not m or not n or not points:
+        return NOT_FOUND
+
+    points.sort(key=lambda p: (p[1], p[0]))
+
+    dp = [[0] * n for _ in range(m)]
+    dp[0][0] = 1
+    k = len(points)
+    i = 0
+
+    while points[i][1] == 0:
+        i += 1
+
+    if i >= k:
+        return NOT_FOUND
+
+    for y in range(1, n):
+        for x in range(m):
+            dp[x][y] = dp[x][y - 1]
+
+            if x > 0:
+                dp[x][y] += dp[x - 1][y - 1]
+
+            if x + 1 < m:
+                dp[x][y] += dp[x + 1][y - 1]
+
+        if i < k and y == points[i][1]:
+            for x in range(m):
+                if x != points[i][0]:
+                    dp[x][y] = 0
+            i += 1
+
+    return dp[0][n - 1] if i == k else NOT_FOUND
+
+
+def find_unique_paths4(m, n, h):
     """
     :type m: int
     :type n: int
     :type h: int
     :rtype: int
 
-    >>> unique_paths_from_three_dir2_3(2, 3, 1)
-    1
-    >>> unique_paths_from_three_dir2_3(3, 3, 1)
-    1
-    >>> unique_paths_from_three_dir2_3(3, 3, 2)
-    0
-    >>> unique_paths_from_three_dir2_3(4, 4, 0)
-    4
-    >>> unique_paths_from_three_dir2_3(4, 4, 1)
-    3
-    >>> unique_paths_from_three_dir2_3(4, 4, 2)
-    0
+    >>> gotcha = [
+    ...     find_unique_paths4(*_in) == _out
+    ...     for _in, _out in (
+    ...         ((2, 3, 1), 1), ((3, 3, 1), 1), ((3, 3, 2), 0),
+    ...         ((4, 4, 0), 4), ((4, 4, 1), 3), ((4, 4, 2), 0),
+    ...         ((6, 7, 0), 51), ((6, 7, 1), 50), ((6, 7, 2), 19),
+    ...         ((6, 7, 3), 1), ((6, 7, 4), 0), ((6, 7, 5), 0)
+    ...     )
+    ... ]
+    >>> bool(gotcha) and all(gotcha)
+    True
     """
     if not m or not n:
         return 0
@@ -329,7 +222,7 @@ def unique_paths_from_three_dir2_3(m, n, h):
             if x + 1 < m:
                 dp[x][y] += dp[x + 1][y - 1]
 
-    if h == 0:
+    if h < 1:
         return dp[0][n - 1]
 
     for y in range(n):
@@ -347,3 +240,38 @@ def unique_paths_from_three_dir2_3(m, n, h):
                 dp[x][y] += dp[x + 1][y - 1]
 
     return dp[0][n - 1]
+
+
+def find_unique_paths5(m, n):
+    """
+    :type m: int
+    :type n: int
+    :rtype: int
+
+    >>> gotcha = [
+    ...     find_unique_paths5(*_in) == _out
+    ...     for _in, _out in (
+    ...         ((2, 2), 1), ((2, 3), 1), ((3, 3), 2),
+    ...         ((5, 5), 9), ((7, 6), 51), ((6, 7), 21),
+    ...     )
+    ... ]
+    >>> bool(gotcha) and all(gotcha)
+    True
+    """
+    if not m or not n:
+        return 0
+
+    dp = [[0] * n for _ in range(m)]
+    dp[0][0] = 1
+
+    for x in range(1, m):
+        for y in range(n):
+            dp[x][y] = dp[x - 1][y]
+
+            if y > 0:
+                dp[x][y] += dp[x - 1][y - 1]
+
+            if y + 1 < n:
+                dp[x][y] += dp[x - 1][y + 1]
+
+    return dp[m - 1][0]
