388ab215e86d88e8d8af8aa31bda51f461d0fb48
Jaych Su <hi.jaychsu@gmail.com>
2018-08-02 11:18:40 +0800

    Fix issue in jest

--- bin/test_javascript.js
+++ bin/test_javascript.js
@@ -1,7 +1,10 @@
 // TODO: wrap this file to a executable command
 // [Configuring Jest](https://facebook.github.io/jest/docs/en/configuration.html)
+
+
 module.exports = {
   "rootDir": "..",
+  "testEnvironment": "node",
   "testRegex": "/(other|topic)/.*(\\.|/)(test|spec)\\.jsx?$",
-  "verbose": true
+  "verbose": true,
 }
